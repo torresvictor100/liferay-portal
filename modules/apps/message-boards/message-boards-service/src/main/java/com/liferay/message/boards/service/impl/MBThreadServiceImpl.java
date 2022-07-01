829de5b7c5ab452697c4bb6d0d13895c4fa25cab
Francisco Lima <francisco.lima@liferay.com>
2022-07-01 16:35:06 -0300

    LPS-157305 @Override added to getSectionNotAnsweredThreads method in remote service

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBThreadServiceImpl.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBThreadServiceImpl.java
@@ -311,8 +311,12 @@ public class MBThreadServiceImpl extends MBThreadServiceBaseImpl {
 			groupId, userId, categoryIds, false, status);
 	}
 
-	public List<MBThread> getSectionNotAnsweredThreads(long groupId, long categoryId) {
-		return mbThreadLocalService.getSectionNotAnsweredThreads(groupId, categoryId);
+	@Override
+	public List<MBThread> getSectionNotAnsweredThreads(
+		long groupId, long categoryId) {
+
+		return mbThreadLocalService.getSectionNotAnsweredThreads(
+			groupId, categoryId);
 	}
 
 	@Override
