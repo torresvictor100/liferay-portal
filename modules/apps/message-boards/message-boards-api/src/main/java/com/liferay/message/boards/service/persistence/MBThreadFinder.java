fcb07c3df780f835f84635a28c083a2ab7ba4568
João Victor Torres Araujo <torresvictor100@gmail.com>
2022-07-04 10:28:24 -0300

    buildRest

--- modules/apps/message-boards/message-boards-api/src/main/java/com/liferay/message/boards/service/persistence/MBThreadFinder.java
+++ modules/apps/message-boards/message-boards-api/src/main/java/com/liferay/message/boards/service/persistence/MBThreadFinder.java
@@ -96,6 +96,12 @@ public interface MBThreadFinder {
 			com.liferay.portal.kernel.dao.orm.QueryDefinition
 				<com.liferay.message.boards.model.MBThread> queryDefinition);
 
+	public java.util.List<com.liferay.message.boards.model.MBThread>
+		findByNotAnsweredThreads(long groupId);
+
+	public java.util.List<com.liferay.message.boards.model.MBThread>
+		findBySectionNotAnsweredThreads(long groupId, long categoryId);
+
 	public java.util.List<com.liferay.message.boards.model.MBThread> findByG_U(
 		long groupId, long userId,
 		com.liferay.portal.kernel.dao.orm.QueryDefinition
