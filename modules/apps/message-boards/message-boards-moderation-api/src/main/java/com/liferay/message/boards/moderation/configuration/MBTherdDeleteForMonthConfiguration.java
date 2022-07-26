f27fd2996a4399840223963836d8715691655215
João Victor Torres Araujo <torresvictor100@gmail.com>
2022-07-26 06:34:10 -0300

    criandoAInterfaceDeCOnfiguracao

---
+++ modules/apps/message-boards/message-boards-moderation-api/src/main/java/com/liferay/message/boards/moderation/configuration/MBTherdDeleteForMonthConfiguration.java
@@ -0,0 +1,30 @@
+package com.liferay.message.boards.moderation.configuration;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import aQute.bnd.annotation.metatype.Meta;
+
+@ExtendedObjectClassDefinition(
+	category = "message-boards",
+	scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	id = "com.liferay.message.boards.moderation.configuration.MBTherdDeleteForMonthConfiguration",
+	localization = "content/Language",
+	name = "MBTherd Delete For Month Configuration"
+)
+public interface MBTherdDeleteForMonthConfiguration {
+
+
+	@Meta.AD(
+		deflt = "false", description = "enable message boards deleta automatico",
+		name = "enable message boards deleta automatico", required = false
+	)
+	public boolean enableMessageBoardsDeletaAutomatico();
+
+
+	@Meta.AD(
+		deflt = "1", description = "message boards deleta periodo sem resposta",
+		min = "1", name = "message boards deleta periodo sem resposta", required = false
+	)
+	public int messageBoardsDeletaPeriodoSemResposta();
+}
