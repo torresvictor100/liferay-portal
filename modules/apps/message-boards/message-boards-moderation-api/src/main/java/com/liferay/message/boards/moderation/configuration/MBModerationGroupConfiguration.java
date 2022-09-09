cb00c9f4ac0c6c13ab891d24854dfce72edde5fe
João Victor Torres Araujo <torresvictor100@gmail.com>
2022-09-09 09:19:17 -0300

    LPS-162674 Create configuration for badge on moderation panel

--- modules/apps/message-boards/message-boards-moderation-api/src/main/java/com/liferay/message/boards/moderation/configuration/MBModerationGroupConfiguration.java
+++ modules/apps/message-boards/message-boards-moderation-api/src/main/java/com/liferay/message/boards/moderation/configuration/MBModerationGroupConfiguration.java
@@ -50,4 +50,10 @@ public interface MBModerationGroupConfiguration {
 	)
 	public int minimumContributedMessages();
 
+	@Meta.AD(
+		deflt = " ", description = "enter-the-badge-domains",
+		name = "badge-domains", required = false
+	)
+	public String badgeDomains();
+
 }
