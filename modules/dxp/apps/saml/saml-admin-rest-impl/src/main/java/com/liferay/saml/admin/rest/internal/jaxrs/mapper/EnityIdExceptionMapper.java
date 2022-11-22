2d50f0bad6da1abd4a6d61fdccd92eebfa9f16c5
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2022-11-22 14:46:50 +0000

    LPS-152126 Follow up to 5b271748bb. Better if we use individual ExceptionMappers to avoid registering one for PortalException?
    For LPS-152122.

---
+++ modules/dxp/apps/saml/saml-admin-rest-impl/src/main/java/com/liferay/saml/admin/rest/internal/jaxrs/mapper/EnityIdExceptionMapper.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.saml.admin.rest.internal.jaxrs.mapper;
+
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+import com.liferay.saml.runtime.exception.EntityIdException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Saml.Admin.REST)",
+		"osgi.jaxrs.extension=true"
+	},
+	service = ExceptionMapper.class
+)
+public class EnityIdExceptionMapper
+	extends BaseExceptionMapper<EntityIdException> {
+
+	@Override
+	protected Problem getProblem(EntityIdException entityIdException) {
+		return new Problem(
+			Response.Status.BAD_REQUEST, entityIdException.getMessage());
+	}
+
+	@Override
+	protected boolean isSanitize() {
+		return false;
+	}
+
+}
