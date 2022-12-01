e0cd144a7577dde2b4563176c959489f55487e31
drewbrokke <drew.brokke@liferay.com>
2022-12-01 14:29:00 -0600

    LPS-165904 portal-instances-web: props transformer: adds native modal footer buttons

--- modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/PortalInstancesManagementToolbarPropsTransformer.js
+++ modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/PortalInstancesManagementToolbarPropsTransformer.js
@@ -14,13 +14,25 @@
 
 import {openModal} from 'frontend-js-web';
 
-export default function propsTransformer({...otherProps}) {
+export default function propsTransformer({portletNamespace, ...otherProps}) {
 	return {
 		...otherProps,
 		onCreateButtonClick(event, {item}) {
 			event.preventDefault();
 
 			openModal({
+				buttons: [
+					{
+						displayType: 'secondary',
+						label: Liferay.Language.get('cancel'),
+						type: 'cancel',
+					},
+					{
+						formId: `${portletNamespace}fm`,
+						label: Liferay.Language.get('add'),
+						type: 'submit',
+					},
+				],
 				height: '60vh',
 				iframeBodyCssClass: '',
 				size: 'md',
