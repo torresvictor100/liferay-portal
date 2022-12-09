9f0691b0be512910ce3f8aa68ed7e3ae7714d948
Nilton Vieira <nilton.vieira@liferay.com>
2022-12-09 10:10:31 -0300

    LPS-170809 add validation and first steps

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray-dispatch-task-executor/src/main/java/com/liferay/site/initializer/testray/dispatch/task/executor/internal/dispatch/executor/SiteInitializerTestrayAutoFillDispatchTaskExecutor.java
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray-dispatch-task-executor/src/main/java/com/liferay/site/initializer/testray/dispatch/task/executor/internal/dispatch/executor/SiteInitializerTestrayAutoFillDispatchTaskExecutor.java
@@ -17,6 +17,19 @@ package com.liferay.site.initializer.testray.dispatch.task.executor.internal.dis
 import com.liferay.dispatch.executor.DispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
 import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 
 /**
  * @author Nilton Vieira
@@ -38,11 +51,60 @@ public class SiteInitializerTestrayAutoFillDispatchTaskExecutor
 			DispatchTrigger dispatchTrigger,
 			DispatchTaskExecutorOutput dispatchTaskExecutorOutput)
 		throws Exception {
+
+		UnicodeProperties unicodeProperties =
+			dispatchTrigger.getDispatchTaskSettingsUnicodeProperties();
+
+		if (Validator.isNull(unicodeProperties.getProperty("testrayEntity1")) ||
+			Validator.isNull(unicodeProperties.getProperty("testrayEntity2"))) {
+
+			_log.error("The required properties are not set");
+
+			return;
+		}
+
+		User user = _userLocalService.getUser(dispatchTrigger.getUserId());
+
+		defaultDTOConverterContext = new DefaultDTOConverterContext(
+			false, null, null, null, null, LocaleUtil.getSiteDefault(), null,
+			user);
+
+		PermissionChecker originalPermissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		String originalName = PrincipalThreadLocal.getName();
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		try {
+			loadObjectDefinitions(dispatchTrigger.getCompanyId());
+		}
+		finally {
+			PermissionThreadLocal.setPermissionChecker(
+				originalPermissionChecker);
+
+			PrincipalThreadLocal.setName(originalName);
+		}
 	}
 
 	@Override
 	public String getName() {
 		return "testray-autofill";
 	}
+
+	private void _autoFillRuns(
+			long companyId, ObjectEntry testrayRunObjectEntry1,
+			ObjectEntry testrayRunObjectEntry2)
+		throws Exception {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SiteInitializerTestrayAutoFillDispatchTaskExecutor.class);
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
