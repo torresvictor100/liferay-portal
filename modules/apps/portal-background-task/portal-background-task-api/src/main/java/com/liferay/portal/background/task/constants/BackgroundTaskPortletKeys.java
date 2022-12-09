3b4aa01f0a65cb986220e7d470301242598ddabb
Dante Wang <dante.wang@liferay.com>
2022-12-09 15:45:10 +0800

    LPS-158311 Add an empty portlet to support notifications

---
+++ modules/apps/portal-background-task/portal-background-task-api/src/main/java/com/liferay/portal/background/task/constants/BackgroundTaskPortletKeys.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.background.task.constants;
+
+/**
+ * @author Dante Wang
+ */
+public class BackgroundTaskPortletKeys {
+
+	public static final String BACKGROUND_TASK =
+		"com_liferay_portal_background_task_web_internal_portlet_" +
+			"BackgroundTaskPortlet";
+
+}
---
+++ modules/apps/portal-background-task/portal-background-task-web/src/main/java/com/liferay/portal/background/task/web/internal/portlet/BackgroundTaskPortlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.background.task.web.internal.portlet;
+
+import com.liferay.portal.background.task.constants.BackgroundTaskPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Dante Wang
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.display-name=Background Task",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + BackgroundTaskPortletKeys.BACKGROUND_TASK,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,power-user,user",
+		"javax.portlet.version=3.0"
+	},
+	service = Portlet.class
+)
+public class BackgroundTaskPortlet extends MVCPortlet {
+}
