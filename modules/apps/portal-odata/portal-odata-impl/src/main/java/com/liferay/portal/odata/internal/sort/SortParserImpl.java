5f9f1f20e9802b81b814d60a3c832e1d73852dfe
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-16 10:40:10 +0800

    LPS-172269 SortParserImpl: Remove usages of Optional

--- modules/apps/portal-odata/portal-odata-impl/src/main/java/com/liferay/portal/odata/internal/sort/SortParserImpl.java
+++ modules/apps/portal-odata/portal-odata-impl/src/main/java/com/liferay/portal/odata/internal/sort/SortParserImpl.java
@@ -29,7 +29,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Parses {@code Sort} strings. This class uses a model to create a {@code
@@ -76,19 +75,10 @@ public class SortParserImpl implements SortParser {
 		}
 
 		return TransformUtil.transform(
-			StringUtil.split(sortString),
-			s -> {
-				Optional<SortField> sortFieldOptional = getSortFieldOptional(s);
-
-				if (sortFieldOptional.isPresent()) {
-					return sortFieldOptional.get();
-				}
-
-				return null;
-			});
+			StringUtil.split(sortString), s -> getSortField(s));
 	}
 
-	protected Optional<EntityField> getEntityFieldOptional(
+	protected EntityField getEntityField(
 		Map<String, EntityField> entityFieldsMap, String fieldName) {
 
 		if (fieldName.contains(StringPool.FORWARD_SLASH)) {
@@ -110,19 +100,19 @@ public class SortParserImpl implements SortParser {
 			ComplexEntityField complexEntityField =
 				(ComplexEntityField)entityField;
 
-			return getEntityFieldOptional(
+			return getEntityField(
 				complexEntityField.getEntityFieldsMap(),
 				fieldName.substring(complexTypeName.length() + 1));
 		}
 
-		return Optional.ofNullable(entityFieldsMap.get(fieldName));
+		return entityFieldsMap.get(fieldName);
 	}
 
-	protected Optional<SortField> getSortFieldOptional(String sortString) {
+	protected SortField getSortField(String sortString) {
 		List<String> list = StringUtil.split(sortString, ':');
 
 		if (list.isEmpty()) {
-			return Optional.empty();
+			return null;
 		}
 
 		if (list.size() > 2) {
@@ -142,18 +132,18 @@ public class SortParserImpl implements SortParser {
 		}
 
 		if (_entityModel == null) {
-			return Optional.of(new SortField(fieldName, ascending));
+			return new SortField(fieldName, ascending);
 		}
 
-		Optional<EntityField> entityFieldOptional = getEntityFieldOptional(
+		EntityField entityField = getEntityField(
 			_entityModel.getEntityFieldsMap(), fieldName);
 
-		return entityFieldOptional.map(
-			entityField -> Optional.of(new SortField(entityField, ascending))
-		).orElseThrow(
-			() -> new InvalidSortException(
-				"Unable to sort by property: " + fieldName)
-		);
+		if (entityField == null) {
+			throw new InvalidSortException(
+				"Unable to sort by property: " + fieldName);
+		}
+
+		return new SortField(entityField, ascending);
 	}
 
 	protected boolean isAscending(String orderBy) {
