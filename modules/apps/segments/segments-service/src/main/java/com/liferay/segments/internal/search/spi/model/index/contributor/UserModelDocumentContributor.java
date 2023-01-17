91e0640fbd70d53fd84b885d80405aef05388a43
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-17 13:05:42 -0800

    LPS-173075 remove the usage of stream in UserModelDocumentContributor, and use TransformUtil.transformToLongArray()

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/index/contributor/UserModelDocumentContributor.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/index/contributor/UserModelDocumentContributor.java
@@ -14,6 +14,7 @@
 
 package com.liferay.segments.internal.search.spi.model.index.contributor;
 
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
@@ -23,9 +24,6 @@ import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.segments.model.SegmentsEntryRel;
 import com.liferay.segments.service.SegmentsEntryRelLocalService;
-
-import java.util.List;
-import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,15 +56,10 @@ public class UserModelDocumentContributor
 	}
 
 	private long[] _getSegmentsEntryIds(User user) throws Exception {
-		List<SegmentsEntryRel> segmentsEntryRels =
+		return TransformUtil.transformToLongArray(
 			_segmentsEntryRelLocalService.getSegmentsEntryRels(
-				_portal.getClassNameId(User.class), user.getUserId());
-
-		Stream<SegmentsEntryRel> stream = segmentsEntryRels.stream();
-
-		return stream.mapToLong(
-			SegmentsEntryRel::getSegmentsEntryId
-		).toArray();
+				_portal.getClassNameId(User.class), user.getUserId()),
+			SegmentsEntryRel::getSegmentsEntryId);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
