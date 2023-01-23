ca9fe7b0b35080c440cb505bdfc6d421f59664a2
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-23 13:27:40 -0800

    LPS-173503 remove the usage of stream in ComplexQueryBuilderImpl

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
@@ -37,9 +37,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author André de Oliveira
@@ -62,14 +59,16 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 
 	@Override
 	public Query build() {
-		Stream<ComplexQueryPart> stream = _complexQueryParts.stream();
+		Map<String, ComplexQueryPart> complexQueryPartsMap = new HashMap<>();
 
-		Map<String, ComplexQueryPart> complexQueryPartsMap = stream.filter(
-			filterQueryDefinition -> !Validator.isBlank(
-				filterQueryDefinition.getName())
-		).collect(
-			Collectors.toMap(ComplexQueryPart::getName, Function.identity())
-		);
+		for (ComplexQueryPart complexQueryPart : _complexQueryParts) {
+			if (Validator.isBlank(complexQueryPart.getName())) {
+				continue;
+			}
+
+			complexQueryPartsMap.put(
+				complexQueryPart.getName(), complexQueryPart);
+		}
 
 		Build build = new Build(complexQueryPartsMap, _getRootBooleanQuery());
 
