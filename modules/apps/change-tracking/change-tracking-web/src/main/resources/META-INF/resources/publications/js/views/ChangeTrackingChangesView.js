7598741ad8561f63d3b0617f5725ab8810186043
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-01-24 17:27:26 +0100

    LPS-173809 Replace code by ClayEmptyState in ChangeTrackingChangesView

--- modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingChangesView.js
+++ modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingChangesView.js
@@ -16,9 +16,11 @@ import ClayAlert from '@clayui/alert';
 import ClayBreadcrumb from '@clayui/breadcrumb';
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import ClayDropDown, {ClayDropDownWithItems} from '@clayui/drop-down';
+import ClayEmptyState from '@clayui/empty-state';
 import {ClayCheckbox, ClayInput, ClayToggle} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayLabel from '@clayui/label';
+import ClayLayout from '@clayui/layout';
 import ClayLink from '@clayui/link';
 import ClayNavigationBar from '@clayui/navigation-bar';
 import {ClayPaginationBarWithBasicItems} from '@clayui/pagination-bar';
@@ -2688,13 +2690,16 @@ export default function ChangeTrackingChangesView({
 				<div className="container-fluid container-fluid-max-xl">
 					{renderExpiredBanner()}
 
-					<div className="sheet taglib-empty-result-message">
-						<div className="taglib-empty-result-message-header" />
-
-						<div className="sheet-text text-center">
-							{Liferay.Language.get('no-changes-were-found')}
-						</div>
-					</div>
+					<ClayLayout.Sheet>
+						<ClayEmptyState
+							className="mt-0"
+							description={Liferay.Language.get(
+								'no-changes-were-found'
+							)}
+							imgSrc={`${themeDisplay.getPathThemeImages()}/states/empty_state.gif`}
+							title={null}
+						/>
+					</ClayLayout.Sheet>
 				</div>
 			);
 		}
