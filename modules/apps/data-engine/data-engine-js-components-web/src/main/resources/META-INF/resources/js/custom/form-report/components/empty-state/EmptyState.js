0b6ef24c8fb8fd264ce324a66477ef46f173633c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-01-25 14:34:31 +0100

    LPS-173809 Replace code by ClayEmptyState in EmptyState

--- modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form-report/components/empty-state/EmptyState.js
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form-report/components/empty-state/EmptyState.js
@@ -12,6 +12,7 @@
  * details.
  */
 
+import ClayEmptyState from '@clayui/empty-state';
 import React from 'react';
 
 export default function EmptyState({
@@ -21,14 +22,10 @@ export default function EmptyState({
 	title = Liferay.Language.get('there-are-no-entries'),
 }) {
 	return (
-		<div className="empty-message taglib-empty-result-message">
-			<div className="taglib-empty-result-message-header"></div>
-
-			<div className="sheet-text text-center text-muted">
-				<h1 className="text-default">{title}</h1>
-
-				<p className="text-default">{description}</p>
-			</div>
-		</div>
+		<ClayEmptyState
+			description={description}
+			imgSrc={`${themeDisplay.getPathThemeImages()}/states/empty_state.gif`}
+			title={title}
+		/>
 	);
 }
