303d6617b3f7b83cc7366a4466ae20fe89561b84
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-01-26 16:32:53 -0300

    LPS-172562 Call DB instead of elastic when the object is scoped by account - We decided to leave ES because we need to support categories and tags filters when using DB

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/info/collection/provider/ObjectEntrySingleFormVariationInfoCollectionProvider.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/info/collection/provider/ObjectEntrySingleFormVariationInfoCollectionProvider.java
@@ -73,7 +73,6 @@ import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
 import com.liferay.portal.kernel.search.generic.NestedQuery;
 import com.liferay.portal.kernel.search.generic.TermQueryImpl;
@@ -420,8 +419,6 @@ public class ObjectEntrySingleFormVariationInfoCollectionProvider
 
 		Group scopeGroup = themeDisplay.getScopeGroup();
 
-		Pagination pagination = collectionQuery.getPagination();
-
 		Page<com.liferay.object.rest.dto.v1_0.ObjectEntry> objectEntriesPage =
 			objectEntryManager.getObjectEntries(
 				themeDisplay.getCompanyId(), _objectDefinition,
@@ -429,10 +426,9 @@ public class ObjectEntrySingleFormVariationInfoCollectionProvider
 				new DefaultDTOConverterContext(
 					false, null, null, null, null, themeDisplay.getLocale(),
 					null, themeDisplay.getUser()),
-				(Filter)null,
-				com.liferay.portal.vulcan.pagination.Pagination.of(
-					1, pagination.getEnd()),
-				null, null);
+				_getFilter(collectionQuery),
+				_getPagination(collectionQuery.getPagination()),
+				_getSearch(collectionQuery), null);
 
 		List<com.liferay.object.rest.dto.v1_0.ObjectEntry> objectEntries =
 			new ArrayList<>(objectEntriesPage.getItems());
@@ -482,6 +478,49 @@ public class ObjectEntrySingleFormVariationInfoCollectionProvider
 		}
 
 		return "";
+	}
+
+	private String _getFilter(CollectionQuery collectionQuery) {
+		Map<String, String[]> configuration =
+			collectionQuery.getConfiguration();
+
+		List<ObjectField> objectFields =
+			_objectFieldLocalService.getObjectFields(
+				_objectDefinition.getObjectDefinitionId());
+
+		StringBundler sb = new StringBundler();
+
+		for (Map.Entry<String, String[]> entry : configuration.entrySet()) {
+			if (Validator.isNull(entry.getValue()[0])) {
+				continue;
+			}
+
+			ObjectField objectField = _getObjectField(
+				entry.getKey(), objectFields);
+
+			if (objectField == null) {
+				continue;
+			}
+
+			sb.append(entry.getKey());
+			sb.append(" eq ");
+
+			if (Objects.equals(
+					objectField.getBusinessType(),
+					ObjectFieldConstants.BUSINESS_TYPE_BOOLEAN)) {
+
+				sb.append(entry.getValue()[0]);
+			}
+			else {
+				sb.append(StringPool.APOSTROPHE);
+				sb.append(entry.getValue()[0]);
+				sb.append(StringPool.APOSTROPHE);
+			}
+
+			sb.append(" and ");
+		}
+
+		return StringUtil.removeLast(sb.toString(), " and ");
 	}
 
 	private long _getGroupId() throws Exception {
@@ -673,6 +712,32 @@ public class ObjectEntrySingleFormVariationInfoCollectionProvider
 		return options;
 	}
 
+	private com.liferay.portal.vulcan.pagination.Pagination _getPagination(
+		Pagination pagination) {
+
+		int page = 1;
+
+		int pageSize = pagination.getEnd() - pagination.getStart();
+
+		if (pageSize > 0) {
+			page = pagination.getEnd() / pageSize;
+		}
+
+		return com.liferay.portal.vulcan.pagination.Pagination.of(
+			page, pageSize);
+	}
+
+	private String _getSearch(CollectionQuery collectionQuery) {
+		KeywordsInfoFilter keywordsInfoFilter = collectionQuery.getInfoFilter(
+			KeywordsInfoFilter.class);
+
+		if (keywordsInfoFilter != null) {
+			return keywordsInfoFilter.getKeywords();
+		}
+
+		return null;
+	}
+
 	private boolean _hasCategorizationObjectLayoutBox() {
 		ObjectLayout objectLayout = null;
 
@@ -713,6 +778,7 @@ public class ObjectEntrySingleFormVariationInfoCollectionProvider
 
 		serviceBuilderObjectEntry.setExternalReferenceCode(
 			objectEntry.getExternalReferenceCode());
+		serviceBuilderObjectEntry.setObjectEntryId(objectEntry.getId());
 		serviceBuilderObjectEntry.setObjectDefinitionId(objectDefinitionId);
 
 		return serviceBuilderObjectEntry;
