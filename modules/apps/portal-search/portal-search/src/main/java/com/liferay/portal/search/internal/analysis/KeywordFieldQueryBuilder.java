ead841fba5800a0883abb7b19911e580e3a2e2ee
Joshua Cords <joshua.cords@liferay.com>
2023-01-26 11:08:23 -0800

    LPS-173644 portal-search: add KeywordFieldQuery and builder
    https://issues.liferay.com/browse/LPS-173644

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/analysis/KeywordFieldQueryBuilder.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.analysis;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.Query;
+import com.liferay.portal.kernel.search.TermQuery;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
+import com.liferay.portal.kernel.search.generic.WildcardQueryImpl;
+import com.liferay.portal.search.analysis.FieldQueryBuilder;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Joshua Cords
+ */
+@Component(
+	configurationPid = "com.liferay.portal.search.configuration.KeywordFieldQueryBuilderConfiguration",
+	service = KeywordFieldQueryBuilder.class
+)
+public class KeywordFieldQueryBuilder implements FieldQueryBuilder {
+
+	public Query build(String field, String value) {
+		TermQuery termQuery = new TermQueryImpl(field, value);
+
+		if (_boost != null) {
+			termQuery.setBoost(_boost);
+		}
+
+		BooleanQuery booleanQuery = new BooleanQueryImpl();
+
+		try {
+			booleanQuery.add(
+				new WildcardQueryImpl(field, value + StringPool.STAR),
+				BooleanClauseOccur.MUST);
+			booleanQuery.add(termQuery, BooleanClauseOccur.SHOULD);
+		}
+		catch (ParseException parseException) {
+			throw new SystemException(parseException);
+		}
+
+		return booleanQuery;
+	}
+
+	public void setBoost(float boost) {
+		_boost = boost;
+	}
+
+	private Float _boost;
+
+}
---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/analysis/KeywordFieldQueryBuilderFactory.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.analysis;
+
+import com.liferay.portal.kernel.search.query.QueryPreProcessConfiguration;
+import com.liferay.portal.search.analysis.FieldQueryBuilder;
+import com.liferay.portal.search.analysis.FieldQueryBuilderFactory;
+import com.liferay.portal.search.index.IndexInformation;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Joshua Cords
+ */
+@Component(service = FieldQueryBuilderFactory.class)
+public class KeywordFieldQueryBuilderFactory
+	implements FieldQueryBuilderFactory {
+
+	@Override
+	public FieldQueryBuilder getQueryBuilder(String fieldName) {
+		if (queryPreProcessConfiguration.isKeywordField(fieldName)) {
+			return keywordFieldQueryBuilder;
+		}
+
+		return null;
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	protected volatile IndexInformation indexInformation;
+
+	@Reference
+	protected KeywordFieldQueryBuilder keywordFieldQueryBuilder;
+
+	@Reference
+	protected QueryPreProcessConfiguration queryPreProcessConfiguration;
+
+}
