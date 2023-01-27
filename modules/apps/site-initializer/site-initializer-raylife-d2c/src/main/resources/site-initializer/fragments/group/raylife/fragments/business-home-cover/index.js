17a2cc773d129e97b94900a90c70b19226ae97ab
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2023-01-27 18:11:07 -0300

    LPS-173932 Fix Url Path business-home-cover

--- modules/apps/site-initializer/site-initializer-raylife-d2c/src/main/resources/site-initializer/fragments/group/raylife/fragments/business-home-cover/index.js
+++ modules/apps/site-initializer/site-initializer-raylife-d2c/src/main/resources/site-initializer/fragments/group/raylife/fragments/business-home-cover/index.js
@@ -63,7 +63,6 @@ const getQuoteForm = fragmentElement.querySelector('#get-quote-form');
 const newQuoteButton = fragmentElement.querySelector('#new-quote-button');
 const newQuoteContainer = fragmentElement.querySelector('#new-quote');
 const newQuoteFormContainer = fragmentElement.querySelector('.new-quote-form');
-const pathContext = Liferay.ThemeDisplay.getPathContext();
 const retrieveQuoteButton = fragmentElement.querySelector(
 	'#retrieve-quote-button'
 );
@@ -114,8 +113,7 @@ continueQuoteButton.onclick = async function () {
 	}
 
 	const raylifeApplicationResponse = await fetchHeadless(
-		pathContext +
-			`/o/c/raylifeapplications/?filter=email eq '${emailInput.value}'`
+		`o/c/raylifeapplications/?filter=email eq '${emailInput.value}'`
 	);
 
 	if (!raylifeApplicationResponse.items.length) {
