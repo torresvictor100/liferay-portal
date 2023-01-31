08fd01e6c22fa03d97e7ea6f9551f7c31754cce9
Brian I. Kim <il.kim@liferay.com>
2023-01-30 22:18:50 -0800

    COMMERCE-10053 Add new exceptionMappers for Discount

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/jaxrs/exception/mapper/DuplicateCommerceDiscountCommerceAccountGroupRelExceptionMapper.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.pricing.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.discount.exception.DuplicateCommerceDiscountCommerceAccountGroupRelException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian I. Kim
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Pricing)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Pricing.DuplicateCommerceDiscountCommerceAccountGroupRelExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DuplicateCommerceDiscountCommerceAccountGroupRelExceptionMapper
+	extends BaseExceptionMapper
+		<DuplicateCommerceDiscountCommerceAccountGroupRelException> {
+
+	@Override
+	protected Problem getProblem(
+		DuplicateCommerceDiscountCommerceAccountGroupRelException
+			duplicateCommerceDiscountCommerceAccountGroupRelException) {
+
+		return new Problem(
+			Response.Status.CONFLICT, "Duplicate account group relation");
+	}
+
+}
