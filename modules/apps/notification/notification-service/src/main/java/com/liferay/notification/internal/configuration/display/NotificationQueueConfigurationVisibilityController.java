f4c9ee6d358fe25159b3e43a62d9da58e42eea7a
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-01-31 10:14:40 -0300

    LPS-174111 Add feature flag to LPS-155659

---
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/configuration/display/NotificationQueueConfigurationVisibilityController.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notification.internal.configuration.display;
+
+import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import java.io.Serializable;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Murilo Stodolni
+ */
+@Component(
+	property = "configuration.pid=com.liferay.notification.internal.configuration.NotificationQueueConfiguration",
+	service = ConfigurationVisibilityController.class
+)
+public class NotificationQueueConfigurationVisibilityController
+	implements ConfigurationVisibilityController {
+
+	@Override
+	public String getKey() {
+		return "notifications";
+	}
+
+	@Override
+	public boolean isVisible(
+		ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
+
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-155659")) &&
+			(scope == ExtendedObjectClassDefinition.Scope.COMPANY)) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+}
