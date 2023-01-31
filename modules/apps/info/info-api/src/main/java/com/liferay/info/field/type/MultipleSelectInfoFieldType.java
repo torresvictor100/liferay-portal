7c966657d1107f6de2c6a2aff347315ad4f87c11
Victor Galan <victor.galan@liferay.com>
2023-01-31 13:10:21 +0100

    LPS-173885 Add a new info field type for multiple select

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/MultipleSelectInfoFieldType.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.field.type;
+
+import com.liferay.info.localized.InfoLocalizedValue;
+
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * @author Víctor Galán
+ */
+public class MultipleSelectInfoFieldType implements InfoFieldType {
+
+	public static final MultipleSelectInfoFieldType INSTANCE =
+		new MultipleSelectInfoFieldType();
+
+	public static final Attribute
+		<MultipleSelectInfoFieldType, Collection<Option>> OPTIONS =
+			new Attribute<>();
+
+	@Override
+	public String getName() {
+		return "multiple-select";
+	}
+
+	public static class Option {
+
+		public Option(
+			boolean active, InfoLocalizedValue<String> label, String value) {
+
+			_active = active;
+			_label = label;
+			_value = value;
+		}
+
+		public Option(InfoLocalizedValue<String> label, String value) {
+			_label = label;
+			_value = value;
+
+			_active = false;
+		}
+
+		public String getLabel(Locale locale) {
+			return _label.getValue(locale);
+		}
+
+		public String getValue() {
+			return _value;
+		}
+
+		public boolean isActive() {
+			return _active;
+		}
+
+		private final boolean _active;
+		private final InfoLocalizedValue<String> _label;
+		private final String _value;
+
+	}
+
+	private MultipleSelectInfoFieldType() {
+	}
+
+}
