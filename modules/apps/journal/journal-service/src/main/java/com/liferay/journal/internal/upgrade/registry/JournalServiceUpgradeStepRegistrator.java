7d2203729ab287d8a66395503486f0caebd30ba1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-02-01 10:49:57 +0100

    LPS-174322 Upgrade process to removes JournalContentSearch table

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/registry/JournalServiceUpgradeStepRegistrator.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/registry/JournalServiceUpgradeStepRegistrator.java
@@ -353,6 +353,9 @@ public class JournalServiceUpgradeStepRegistrator
 				_layoutLocalService, _layoutClassedModelUsageLocalService,
 				_portletPreferencesLocalService,
 				_portletPreferenceValueLocalService));
+
+		registry.register(
+			"4.4.3", "5.0.0", new JournalContentSearchUpgradeProcess());
 	}
 
 	private void _deleteTempImages() throws Exception {
