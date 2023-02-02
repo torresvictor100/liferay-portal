42400001e3471420bafb9300ff27520a8447ae94
shuyangzhou <shuyang.zhou@liferay.com>
2023-02-01 19:51:32 -0800

    LPS-173535 Ensure memory visibilty. This is one of those rare cases where failed to use volatile caused real sneaky bugs.

--- portal-impl/src/com/liferay/portal/events/StartupHelperUtil.java
+++ portal-impl/src/com/liferay/portal/events/StartupHelperUtil.java
@@ -190,7 +190,7 @@ public class StartupHelperUtil {
 	private static final Log _log = LogFactoryUtil.getLog(
 		StartupHelperUtil.class);
 
-	private static boolean _dbNew;
+	private static volatile boolean _dbNew;
 	private static boolean _startupFinished;
 	private static boolean _upgraded;
 	private static boolean _upgrading;
