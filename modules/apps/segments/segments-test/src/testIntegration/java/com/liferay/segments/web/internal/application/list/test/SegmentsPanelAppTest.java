feb15261ed4233eb365f9cdd641a991e80bf39ca
Cristina González Castellano <cristina.gonzalez@liferay.com>
2023-02-02 08:32:51 +0000

    LPS-174133 Add tests.

--- modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/web/internal/application/list/test/SegmentsPanelAppTest.java
+++ modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/web/internal/application/list/test/SegmentsPanelAppTest.java
@@ -17,8 +17,17 @@ package com.liferay.segments.web.internal.application.list.test;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.segments.constants.SegmentsPortletKeys;
 
@@ -112,8 +121,44 @@ public class SegmentsPanelAppTest {
 			SegmentsPortletKeys.SEGMENTS, _panelApp.getPortletId());
 	}
 
+	@Test
+	public void testIsShow() throws PortalException {
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(TestPropsValues.getUser());
+
+		Assert.assertTrue(
+			_panelApp.isShow(
+				permissionChecker,
+				_groupLocalService.getGroup(TestPropsValues.getGroupId())));
+	}
+
+	@Test
+	public void testIsShowWithoutPermissions() throws Exception {
+		User user = UserTestUtil.addUser(TestPropsValues.getGroupId());
+
+		try {
+			PermissionChecker permissionChecker =
+				PermissionCheckerFactoryUtil.create(user);
+
+			Assert.assertFalse(
+				_panelApp.isShow(
+					permissionChecker,
+					_groupLocalService.getGroup(TestPropsValues.getGroupId())));
+		}
+		finally {
+			_userLocalService.deleteUser(user);
+		}
+	}
+
 	private BundleContext _bundleContext;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
 	private PanelApp _panelApp;
 	private ServiceReference<PanelApp> _serviceReference;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
