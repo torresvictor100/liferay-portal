76aa36ba8e15129afb11d43db95e8f007257d4ef
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-02-02 16:10:32 +0000

    LPS-172480 Removes no needed method

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditDDMTemplateDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditDDMTemplateDisplayContext.java
@@ -23,8 +23,6 @@ import com.liferay.journal.configuration.JournalFileUploadsConfiguration;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.web.internal.configuration.JournalWebConfiguration;
 import com.liferay.journal.web.internal.helper.JournalDDMTemplateHelper;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanParamUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -295,14 +293,6 @@ public class JournalEditDDMTemplateDisplayContext {
 		return ResourceBundleUtil.getBundle(themeDisplay.getLocale(), clazz);
 	}
 
-	public String getTemplateLanguageTypeLabel(String templateLanguageType) {
-		return StringBundler.concat(
-			LanguageUtil.get(
-				_httpServletRequest, templateLanguageType + "[stands-for]"),
-			StringPool.SPACE, StringPool.OPEN_PARENTHESIS, StringPool.PERIOD,
-			templateLanguageType, StringPool.CLOSE_PARENTHESIS);
-	}
-
 	public JSONArray getTemplateVariableGroupJSONArray() throws Exception {
 		JournalDDMTemplateHelper journalDDMTemplateHelper =
 			(JournalDDMTemplateHelper)_httpServletRequest.getAttribute(
@@ -405,11 +395,6 @@ public class JournalEditDDMTemplateDisplayContext {
 
 	public String[] imageExtensions() {
 		return _journalFileUploadsConfiguration.imageExtensions();
-	}
-
-	public boolean isAutocompleteEnabled() {
-		return _ddmTemplateHelper.isAutocompleteEnabled(
-			TemplateConstants.LANG_TYPE_FTL);
 	}
 
 	public boolean isCacheable() {
