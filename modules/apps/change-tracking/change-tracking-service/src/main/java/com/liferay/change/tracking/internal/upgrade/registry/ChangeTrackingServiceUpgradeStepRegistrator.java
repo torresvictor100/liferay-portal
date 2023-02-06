b397f8c8d76b0b0f67ad8f5399f62ef0e2dfd5fd
David Truong <david.truong@liferay.com>
2023-02-06 13:24:21 -0600

    LPS-171364 upgrade process for CTProcess

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/upgrade/registry/ChangeTrackingServiceUpgradeStepRegistrator.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/upgrade/registry/ChangeTrackingServiceUpgradeStepRegistrator.java
@@ -16,6 +16,7 @@ package com.liferay.change.tracking.internal.upgrade.registry;
 
 import com.liferay.change.tracking.internal.upgrade.v2_3_0.UpgradeCompanyId;
 import com.liferay.change.tracking.internal.upgrade.v2_4_0.CTSchemaVersionUpgradeProcess;
+import com.liferay.change.tracking.internal.upgrade.v2_7_0.CTProcessUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
@@ -67,6 +68,8 @@ public class ChangeTrackingServiceUpgradeStepRegistrator
 			"2.5.1", "2.6.0",
 			new com.liferay.change.tracking.internal.upgrade.v2_6_0.
 				SchemaUpgradeProcess());
+
+		registry.register("2.6.0", "2.7.0", new CTProcessUpgradeProcess());
 	}
 
 }
