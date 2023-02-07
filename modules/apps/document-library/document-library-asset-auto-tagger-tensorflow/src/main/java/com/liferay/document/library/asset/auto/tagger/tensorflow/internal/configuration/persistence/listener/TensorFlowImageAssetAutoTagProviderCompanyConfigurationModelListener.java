946e1b5113c831cd6a4740d9aa2514905d939b6f
shuyangzhou <shuyang.zhou@liferay.com>
2023-02-07 08:30:22 -0800

    LPS-173441 Move destination registration logic to listener

--- modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/configuration/persistence/listener/TensorFlowImageAssetAutoTagProviderCompanyConfigurationModelListener.java
+++ modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/configuration/persistence/listener/TensorFlowImageAssetAutoTagProviderCompanyConfigurationModelListener.java
@@ -18,20 +18,12 @@ import com.liferay.document.library.asset.auto.tagger.tensorflow.internal.config
 import com.liferay.document.library.asset.auto.tagger.tensorflow.internal.constants.TensorFlowDestinationNames;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
-import com.liferay.portal.kernel.messaging.Destination;
-import com.liferay.portal.kernel.messaging.DestinationConfiguration;
-import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBus;
-import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.Dictionary;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -60,32 +52,6 @@ public class
 				new Message());
 		}
 	}
-
-	@Activate
-	protected void activate(BundleContext bundleContext) {
-		DestinationConfiguration destinationConfiguration =
-			new DestinationConfiguration(
-				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
-				TensorFlowDestinationNames.TENSORFLOW_MODEL_DOWNLOAD);
-
-		Destination destination = _destinationFactory.createDestination(
-			destinationConfiguration);
-
-		_destinationServiceRegistration = bundleContext.registerService(
-			Destination.class, destination,
-			MapUtil.singletonDictionary(
-				"destination.name", destination.getName()));
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_destinationServiceRegistration.unregister();
-	}
-
-	@Reference
-	private DestinationFactory _destinationFactory;
-
-	private ServiceRegistration<Destination> _destinationServiceRegistration;
 
 	@Reference
 	private MessageBus _messageBus;
--- modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/messaging/TensorFlowDownloadMessageListener.java
+++ modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/messaging/TensorFlowDownloadMessageListener.java
@@ -19,14 +19,22 @@ import com.liferay.document.library.asset.auto.tagger.tensorflow.internal.consta
 import com.liferay.document.library.asset.auto.tagger.tensorflow.internal.util.TensorFlowDownloadUtil;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.Map;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alejandro Tardín
@@ -39,12 +47,28 @@ import org.osgi.service.component.annotations.Modified;
 public class TensorFlowDownloadMessageListener extends BaseMessageListener {
 
 	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		_tensorFlowImageAssetAutoTagProviderDownloadConfiguration =
-			ConfigurableUtil.createConfigurable(
-				TensorFlowImageAssetAutoTagProviderDownloadConfiguration.class,
-				properties);
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		modified(properties);
+
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
+				TensorFlowDestinationNames.TENSORFLOW_MODEL_DOWNLOAD);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		_destinationServiceRegistration = bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", destination.getName()));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_destinationServiceRegistration.unregister();
 	}
 
 	@Override
@@ -53,6 +77,18 @@ public class TensorFlowDownloadMessageListener extends BaseMessageListener {
 			_tensorFlowImageAssetAutoTagProviderDownloadConfiguration);
 	}
 
+	@Modified
+	protected void modified(Map<String, Object> properties) {
+		_tensorFlowImageAssetAutoTagProviderDownloadConfiguration =
+			ConfigurableUtil.createConfigurable(
+				TensorFlowImageAssetAutoTagProviderDownloadConfiguration.class,
+				properties);
+	}
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private ServiceRegistration<Destination> _destinationServiceRegistration;
 	private volatile TensorFlowImageAssetAutoTagProviderDownloadConfiguration
 		_tensorFlowImageAssetAutoTagProviderDownloadConfiguration;
 
