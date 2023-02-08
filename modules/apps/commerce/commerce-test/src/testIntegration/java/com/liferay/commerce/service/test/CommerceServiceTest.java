f194e78388898a6ac559a80ae262dbdc0d0bd246
Brian I. Kim <il.kim@liferay.com>
2023-02-08 12:34:32 -0800

    COMMERCE-10054 Add integration tests to reflect changes

---
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/service/test/CommerceServiceTest.java
@@ -0,0 +1,301 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.service.test;
+
+import com.liferay.account.service.AccountEntryUserRelLocalService;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.account.service.CommerceAccountLocalService;
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
+import com.liferay.commerce.product.constants.CommerceChannelConstants;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.product.service.CommerceChannelLocalServiceUtil;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupRoleLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.frutilla.FrutillaRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Brian I. Kim
+ */
+@RunWith(Arquillian.class)
+public class CommerceServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_company = CompanyLocalServiceUtil.getCompany(_group.getCompanyId());
+
+		_user = UserTestUtil.addUser(_company);
+
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			_group.getCompanyId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		_commerceChannel = CommerceChannelLocalServiceUtil.addCommerceChannel(
+			null, _group.getGroupId(), "Test Channel",
+			CommerceChannelConstants.CHANNEL_TYPE_SITE, null,
+			_commerceCurrency.getCode(), _serviceContext);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_commerceOrderLocalService.deleteCommerceOrders(
+			_commerceChannel.getGroupId());
+	}
+
+	@Test
+	public void testAddB2BCommerceOrderByBusinessAccountWithBuyerRole()
+		throws Exception {
+
+		frutillaRule.scenario(
+			"Adding a new order"
+		).given(
+			"A user"
+		).when(
+			"The user has a buyer role"
+		).and(
+			"The user attempts to add an order using a business account"
+		).then(
+			"The order should be successfully created"
+		);
+
+		Role role = _addBuyerRole();
+
+		User user = UserTestUtil.addUser(_company);
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		CommerceAccount commerceAccount =
+			_commerceAccountLocalService.addBusinessCommerceAccount(
+				"Test Business Account", 0, null, null, true, null,
+				new long[] {_user.getUserId()},
+				new String[] {_user.getEmailAddress()}, _serviceContext);
+
+		_accountEntryUserRelLocalService.addAccountEntryUserRel(
+			commerceAccount.getCommerceAccountId(), user.getUserId());
+
+		_userGroupRoleLocalService.addUserGroupRole(
+			user.getUserId(), commerceAccount.getCommerceAccountGroupId(),
+			role.getRoleId());
+
+		_commerceOrderService.addCommerceOrder(
+			_commerceChannel.getGroupId(),
+			commerceAccount.getCommerceAccountId(),
+			_commerceCurrency.getCommerceCurrencyId(), 0);
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testAddB2BCommerceOrderByBusinessAccountWithoutBuyerRole()
+		throws Exception {
+
+		frutillaRule.scenario(
+			"Adding a new order"
+		).given(
+			"A user"
+		).when(
+			"The user does not have a buyer role"
+		).and(
+			"The user attempts to add an order using a business account"
+		).then(
+			"The order should not be created"
+		);
+
+		User user = UserTestUtil.addUser(_company);
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		CommerceAccount commerceAccount =
+			_commerceAccountLocalService.addBusinessCommerceAccount(
+				"Test Business Account", 0, null, null, true, null,
+				new long[] {_user.getUserId()},
+				new String[] {_user.getEmailAddress()}, _serviceContext);
+
+		_accountEntryUserRelLocalService.addAccountEntryUserRel(
+			commerceAccount.getCommerceAccountId(), user.getUserId());
+
+		_commerceOrderService.addCommerceOrder(
+			_commerceChannel.getGroupId(),
+			commerceAccount.getCommerceAccountId(),
+			_commerceCurrency.getCommerceCurrencyId(), 0);
+	}
+
+	@Test
+	public void testAddB2CCommerceOrderByGuestAccount() throws Exception {
+		frutillaRule.scenario(
+			"Adding a new order"
+		).given(
+			"A user"
+		).when(
+			"The user attempts to add a B2C order as a guest"
+		).then(
+			"The order should be successfully created"
+		);
+
+		User user = _company.getDefaultUser();
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		CommerceAccount commerceAccount =
+			_commerceAccountLocalService.getGuestCommerceAccount(
+				_company.getCompanyId());
+
+		_commerceOrderService.addCommerceOrder(
+			_commerceChannel.getGroupId(),
+			commerceAccount.getCommerceAccountId(),
+			_commerceCurrency.getCommerceCurrencyId(), 0);
+	}
+
+	@Test
+	public void testAddB2CCommerceOrderByPersonalAccount() throws Exception {
+		frutillaRule.scenario(
+			"Adding a new order"
+		).given(
+			"A user"
+		).when(
+			"The user attempts to add a B2C order using a personal account"
+		).then(
+			"The order should be successfully created"
+		);
+
+		User user = UserTestUtil.addUser(_company);
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		CommerceAccount commerceAccount =
+			_commerceAccountLocalService.getPersonalCommerceAccount(
+				user.getUserId());
+
+		_commerceOrderService.addCommerceOrder(
+			_commerceChannel.getGroupId(),
+			commerceAccount.getCommerceAccountId(),
+			_commerceCurrency.getCommerceCurrencyId(), 0);
+	}
+
+	@Rule
+	public FrutillaRule frutillaRule = new FrutillaRule();
+
+	private Role _addBuyerRole() throws Exception {
+		Role role = _roleLocalService.addRole(
+			_user.getUserId(), null, 0, "Test Buyer",
+			HashMapBuilder.put(
+				_serviceContext.getLocale(), "Test Buyer"
+			).build(),
+			null, 1, null, _serviceContext);
+
+		_resourcePermissionLocalService.addResourcePermission(
+			_serviceContext.getCompanyId(),
+			CommerceOrderConstants.RESOURCE_NAME, 1,
+			String.valueOf(role.getCompanyId()), role.getRoleId(),
+			"ADD_COMMERCE_ORDER");
+
+		return role;
+	}
+
+	private static Company _company;
+
+	@Inject
+	private AccountEntryUserRelLocalService _accountEntryUserRelLocalService;
+
+	@Inject
+	private CommerceAccountLocalService _commerceAccountLocalService;
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@Inject
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@DeleteAfterTestRun
+	private CommerceCurrency _commerceCurrency;
+
+	@Inject
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Inject
+	private CommerceOrderService _commerceOrderService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private User _user;
+
+	@Inject
+	private UserGroupRoleLocalService _userGroupRoleLocalService;
+
+}
