fb0f2d0953c00a77f7f019ea608f105c653a7f7d
drewbrokke <drew.brokke@liferay.com>
2023-02-08 14:09:27 -0600

    LPS-175159 portal-company-log-web: replaces feature flag reads with FeatureFlagManagerUtil

--- modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/application/list/PortalCompanyLogPanelApp.java
+++ modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/application/list/PortalCompanyLogPanelApp.java
@@ -19,6 +19,7 @@ import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.company.log.web.internal.constants.PortalCompanyLogPortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -52,7 +53,7 @@ public class PortalCompanyLogPanelApp extends BasePanelApp {
 
 		if (!GetterUtil.getBoolean(
 				PropsUtil.get(PropsKeys.COMPANY_LOG_ENABLED)) ||
-			!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-170670"))) {
+			!FeatureFlagManagerUtil.isEnabled("LPS-170670")) {
 
 			return false;
 		}
