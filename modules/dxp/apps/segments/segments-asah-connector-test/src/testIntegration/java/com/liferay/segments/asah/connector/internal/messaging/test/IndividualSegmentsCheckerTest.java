52c5ea279c0c6a36478ac6ddda09258d4db13d89
Cristina González Castellano <cristina.gonzalez@liferay.com>
2023-02-08 11:54:21 +0000

    LPS-172555 We should inject the component directly using OSGi, your approach is correct for unit test, but as we are using integration tests, we can use the component and make things easier.

--- modules/dxp/apps/segments/segments-asah-connector-test/src/testIntegration/java/com/liferay/segments/asah/connector/internal/messaging/test/IndividualSegmentsCheckerTest.java
+++ modules/dxp/apps/segments/segments-asah-connector-test/src/testIntegration/java/com/liferay/segments/asah/connector/internal/messaging/test/IndividualSegmentsCheckerTest.java
@@ -15,15 +15,12 @@
 package com.liferay.segments.asah.connector.internal.messaging.test;
 
 import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
-import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.function.UnsafeSupplier;
 import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
@@ -33,7 +30,6 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -43,11 +39,9 @@ import com.liferay.segments.model.SegmentsEntryRel;
 import com.liferay.segments.service.SegmentsEntryLocalService;
 import com.liferay.segments.service.SegmentsEntryRelLocalService;
 
-import java.lang.reflect.Constructor;
-
 import java.util.List;
-import java.util.Objects;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -56,8 +50,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Mikel Lorza
@@ -76,7 +70,28 @@ public class IndividualSegmentsCheckerTest {
 	public void setUp() throws Exception {
 		_user = TestPropsValues.getUser();
 
-		_setUpIndividualSegmentsChecker();
+		Bundle bundle = FrameworkUtil.getBundle(
+			IndividualSegmentsCheckerTest.class);
+
+		_serviceTracker = new ServiceTracker<>(
+			bundle.getBundleContext(),
+			FrameworkUtil.createFilter(
+				"(component.name=com.liferay.segments.asah.connector." +
+					"internal.messaging.IndividualSegmentsChecker)"),
+			null);
+
+		_serviceTracker.open();
+
+		_individualSegmentsChecker = _serviceTracker.getService();
+
+		Assert.assertNotNull(_individualSegmentsChecker);
+	}
+
+	@After
+	public void tearDown() {
+		if (_serviceTracker != null) {
+			_serviceTracker.close();
+		}
 	}
 
 	@Test
@@ -197,87 +212,7 @@ public class IndividualSegmentsCheckerTest {
 		}
 	}
 
-	private Object _getInstance(String className, Bundle bundle)
-		throws Exception {
-
-		Class<?> clazz = bundle.loadClass(className);
-
-		Constructor<?> constructor = clazz.getConstructor();
-
-		return constructor.newInstance();
-	}
-
-	private void _setUpIndividualSegmentsChecker() throws Exception {
-		Bundle bundle1 = FrameworkUtil.getBundle(
-			IndividualSegmentsCheckerTest.class);
-
-		BundleContext bundleContext = bundle1.getBundleContext();
-
-		Bundle bundle2 = null;
-
-		for (Bundle curBundle : bundleContext.getBundles()) {
-			if (Objects.equals(
-					curBundle.getSymbolicName(),
-					"com.liferay.segments.asah.connector")) {
-
-				bundle2 = curBundle;
-
-				break;
-			}
-		}
-
-		Assert.assertNotNull(
-			"Unable to find com.liferay.segments.asah.connector bundle",
-			bundle2);
-
-		_individualSegmentsChecker = _getInstance(
-			"com.liferay.segments.asah.connector.internal.messaging." +
-				"IndividualSegmentsChecker",
-			bundle2);
-
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_analyticsSettingsManager",
-			_analyticsSettingsManager);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_asahSegmentsEntryCache",
-			_getInstance(
-				"com.liferay.segments.asah.connector.internal.cache." +
-					"AsahSegmentsEntryCache",
-				bundle2));
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_companyLocalService",
-			_companyLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_moduleServiceLifecycle",
-			_moduleServiceLifecycle);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_portal", _portal);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_segmentsEntryLocalService",
-			_segmentsEntryLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_segmentsEntryRelLocalService",
-			_segmentsEntryRelLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_individualSegmentsChecker, "_userLocalService", _userLocalService);
-
-		ReflectionTestUtil.invoke(
-			_individualSegmentsChecker, "activate", new Class<?>[0]);
-	}
-
-	@Inject
-	private AnalyticsSettingsManager _analyticsSettingsManager;
-
-	@Inject
-	private CompanyLocalService _companyLocalService;
-
 	private Object _individualSegmentsChecker;
-
-	@Inject
-	private ModuleServiceLifecycle _moduleServiceLifecycle;
-
-	@Inject
-	private Portal _portal;
 
 	@Inject
 	private SegmentsEntryLocalService _segmentsEntryLocalService;
@@ -285,6 +220,7 @@ public class IndividualSegmentsCheckerTest {
 	@Inject
 	private SegmentsEntryRelLocalService _segmentsEntryRelLocalService;
 
+	private ServiceTracker<Object, Object> _serviceTracker;
 	private User _user;
 
 	@Inject
