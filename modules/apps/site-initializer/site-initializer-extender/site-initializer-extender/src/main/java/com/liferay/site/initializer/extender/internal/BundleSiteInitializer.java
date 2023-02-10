5ee95349e2ad01f40a6049bb93dba3ca40b356c2
João Victor Torres Araujo <torresvictor100@gmail.com>
2023-02-10 17:05:06 -0300

    saexta

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -2452,7 +2452,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 						listTypeDefinition);
 
 			}
-			ListTypeEntry[] ts = listTypeDefinition.getListTypeEntries();
+			ListTypeEntry[] listTypeEntries = listTypeDefinition.getListTypeEntries();
 
 
 
@@ -2483,26 +2483,23 @@ public class BundleSiteInitializer implements SiteInitializer {
 				ListTypeEntry listTypeEntry = ListTypeEntry.toDTO(
 					String.valueOf(jsonArray.getJSONObject(i)));
 
-				Page<ListTypeEntry> listTypeEntriesPage =
-					listTypeEntryResource.
-						getListTypeDefinitionListTypeEntriesPage(
-							listTypeDefinition.getId(), null, null,
-							listTypeEntryResource.toFilter(
-								StringBundler.concat(
-									"key eq '", listTypeEntry.getKey(), "'")),
-							null, null);
+				for(ListTypeEntry listTypeEntrie: listTypeEntries){
 
-				ListTypeEntry existingListTypeEntry =
-					listTypeEntriesPage.fetchFirstItem();
+					if (listTypeEntrie.getKey().equals(listTypeEntry.getKey())) {
+						System.out.println("to sendo chemado ");
+						listTypeEntryResource.putListTypeEntry(listTypeEntrie.getId(),listTypeEntry );
+					}
+					else {
+						System.out.println("to sendo chemado ");
+						listTypeEntryResource.postListTypeDefinitionListTypeEntry(
+							listTypeEntrie.getId(), listTypeEntry);
 
-				if (existingListTypeEntry == null) {
-					listTypeEntryResource.postListTypeDefinitionListTypeEntry(
-						listTypeDefinition.getId(), listTypeEntry);
+					}
+
 				}
-				else {
-					listTypeEntryResource.putListTypeEntry(
-						existingListTypeEntry.getId(), listTypeEntry);
-				}
+
+
+
 			}
 		}
 
