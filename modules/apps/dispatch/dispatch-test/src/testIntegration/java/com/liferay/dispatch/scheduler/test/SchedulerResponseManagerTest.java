cf6c1c424eb008e06c9bfca6c1b02359bb6c8ea8
Tina Tian <tina.tian@liferay.com>
2023-02-10 15:49:55 -0800

    LPS-171602 Fix integration test, assert logic inside message listener may not be invoked

--- modules/apps/dispatch/dispatch-test/src/testIntegration/java/com/liferay/dispatch/scheduler/test/SchedulerResponseManagerTest.java
+++ modules/apps/dispatch/dispatch-test/src/testIntegration/java/com/liferay/dispatch/scheduler/test/SchedulerResponseManagerTest.java
@@ -18,19 +18,21 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dispatch.scheduler.SchedulerResponseManager;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
-import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
-import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.StorageType;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
-import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -39,6 +41,11 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Luis Ortiz
@@ -53,29 +60,71 @@ public class SchedulerResponseManagerTest {
 
 	@Test
 	public void testRun() throws Exception {
-		_company = CompanyTestUtil.addCompany();
+		Bundle bundle = FrameworkUtil.getBundle(
+			SchedulerResponseManagerTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Destination destination = _destinationFactory.createDestination(
+			DestinationConfiguration.createSynchronousDestinationConfiguration(
+				_TEST_DESTINATION_NAME));
+
+		ServiceRegistration<?> serviceRegistration =
+			bundleContext.registerService(
+				Destination.class, destination,
+				HashMapDictionaryBuilder.<String, Object>put(
+					"destination.name", _TEST_DESTINATION_NAME
+				).build());
 
 		TestMessageListener testMessageListener = new TestMessageListener();
 
-		testMessageListener.init();
+		destination.register(testMessageListener);
+
+		_schedulerEngineHelper.register(
+			testMessageListener,
+			new SchedulerEntryImpl(
+				_TEST_NAME,
+				_triggerFactory.createTrigger(
+					_TEST_NAME, _TEST_NAME, null, null, 60, TimeUnit.MINUTE)),
+			_TEST_DESTINATION_NAME);
+
+		_company = CompanyTestUtil.addCompany();
 
 		try (SafeCloseable safeCloseable =
 				CompanyThreadLocal.setWithSafeCloseable(
 					_company.getCompanyId())) {
 
 			_schedulerResponseManager.run(
-				CompanyThreadLocal.getCompanyId(),
-				TestMessageListener.class.getName(),
-				TestMessageListener.class.getName(),
+				CompanyThreadLocal.getCompanyId(), _TEST_NAME, _TEST_NAME,
 				StorageType.MEMORY_CLUSTERED);
+
+			Assert.assertEquals(
+				_company.getCompanyId(),
+				testMessageListener.getCompanyIdFromCompanyThreadLocal());
+			Assert.assertEquals(
+				_company.getCompanyId(),
+				testMessageListener.getCompanyIdFromMessage());
 		}
 		finally {
-			testMessageListener.destroy();
+			serviceRegistration.unregister();
+
+			_schedulerEngineHelper.unregister(testMessageListener);
+
 			CompanyLocalServiceUtil.deleteCompany(_company);
 		}
 	}
 
+	private static final String _TEST_DESTINATION_NAME = "test.destination";
+
+	private static final String _TEST_NAME = "SchedulerResponseManagerTest";
+
 	private static Company _company;
+
+	@Inject
+	private DestinationFactory _destinationFactory;
+
+	@Inject
+	private MessageBus _messageBus;
 
 	@Inject
 	private SchedulerEngineHelper _schedulerEngineHelper;
@@ -88,35 +137,24 @@ public class SchedulerResponseManagerTest {
 
 	private class TestMessageListener extends BaseMessageListener {
 
-		public void destroy() {
-			_schedulerEngineHelper.unregister(this);
+		public long getCompanyIdFromCompanyThreadLocal() {
+			return _companyIdFromCompanyThreadLocal;
 		}
 
-		public void init() {
-			Class<?> clazz = getClass();
-
-			String className = clazz.getName();
-
-			Trigger trigger = _triggerFactory.createTrigger(
-				className, className, null, null, 60, TimeUnit.MINUTE);
-
-			SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-				className, trigger);
-
-			_schedulerEngineHelper.register(
-				this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
+		public long getCompanyIdFromMessage() {
+			return _companyIdFromMessage;
 		}
 
 		@Override
-		protected void doReceive(Message message) throws Exception {
-			long companyId = CompanyThreadLocal.getCompanyId();
+		protected void doReceive(Message message) {
+			_companyIdFromCompanyThreadLocal =
+				CompanyThreadLocal.getCompanyId();
 
-			long messageCompanyId = message.getLong("companyId");
-
-			Assert.assertEquals(_company.getCompanyId(), companyId);
-
-			Assert.assertEquals(_company.getCompanyId(), messageCompanyId);
+			_companyIdFromMessage = message.getLong("companyId");
 		}
+
+		private long _companyIdFromCompanyThreadLocal;
+		private long _companyIdFromMessage;
 
 	}
 
