fe7546fa3190c38e0caa0b1141d786bcb031fbe4
Jiefeng Wu <jeffwu0724@gmail.com>
2023-02-13 13:17:44 -0800

    LPS-175644 remove stream of usage in KQLQuery

--- modules/dxp/apps/sharepoint-rest/sharepoint-rest-repository/src/main/java/com/liferay/sharepoint/rest/repository/internal/search/kql/KQLQuery.java
+++ modules/dxp/apps/sharepoint-rest/sharepoint-rest-repository/src/main/java/com/liferay/sharepoint/rest/repository/internal/search/kql/KQLQuery.java
@@ -14,20 +14,19 @@
 
 package com.liferay.sharepoint.rest.repository.internal.search.kql;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 /**
  * @author Adolfo Pérez
  */
 public interface KQLQuery {
 
 	public static KQLQuery and(KQLQuery... kqlQueries) {
-		Stream<KQLQuery> queryStream = Arrays.stream(kqlQueries);
+		KQLQuery finalKQLQuery = NullKQLQuery.INSTANCE;
 
-		return queryStream.reduce(
-			NullKQLQuery.INSTANCE,
-			(kqlQuery1, kqlQuery2) -> kqlQuery1.and(kqlQuery2));
+		for (KQLQuery kqlQuery : kqlQueries) {
+			finalKQLQuery = finalKQLQuery.and(kqlQuery);
+		}
+
+		return finalKQLQuery;
 	}
 
 	public static KQLQuery eq(String field, String value) {
