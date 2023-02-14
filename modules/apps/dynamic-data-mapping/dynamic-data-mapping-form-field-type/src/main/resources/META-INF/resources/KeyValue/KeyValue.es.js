d85b80fe4be1a5db43c05060f36d69d8444f58f9
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2023-02-14 17:18:54 -0300

    LPS-167798 Add aria-label with new key to close button to announce it's action

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/KeyValue/KeyValue.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/KeyValue/KeyValue.es.js
@@ -14,6 +14,7 @@
 
 import ClayIcon from '@clayui/icon';
 import {normalizeFieldName} from 'data-engine-js-components-web';
+import {sub} from 'frontend-js-web';
 import React, {useRef} from 'react';
 
 import {FieldBase} from '../FieldBase/ReactFieldBase.es';
@@ -124,6 +125,10 @@ const Main = ({
 
 			{showCloseButton && (
 				<button
+					aria-label={sub(
+						Liferay.Language.get('remove-x-option'),
+						keyword
+					)}
 					className="close close-modal"
 					onClick={onClick}
 					type="button"
