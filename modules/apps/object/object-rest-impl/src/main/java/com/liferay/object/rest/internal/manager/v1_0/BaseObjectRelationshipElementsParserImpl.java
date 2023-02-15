a3e1e63278729ae8210af479ee09231b9b6d6ba4
Carlos Correa <carlos.correa@liferay.com>
2023-02-15 13:15:38 +0100

    LPS-172037 Add implementations of the parser for OneToMany and ManyToMany relationships for the ObjectDefinitions that are not system object
    Co-authored-by: Sergio Jiménez del Coso <sergio.jdelcoso@liferay.com>

---
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/BaseObjectRelationshipElementsParserImpl.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.rest.internal.manager.v1_0;
+
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.rest.manager.v1_0.ObjectRelationshipElementsParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.BadRequestException;
+
+/**
+ * @author Carlos Correa
+ * @author Sergio Jimenez del Coso
+ */
+public abstract class BaseObjectRelationshipElementsParserImpl<T>
+	implements ObjectRelationshipElementsParser {
+
+	public BaseObjectRelationshipElementsParserImpl(
+		ObjectDefinition objectDefinition) {
+
+		this.objectDefinition = objectDefinition;
+	}
+
+	@Override
+	public String getClassName() {
+		return objectDefinition.getClassName();
+	}
+
+	protected List<T> parseMany(Object item) {
+		if (!(item instanceof List)) {
+			throw new BadRequestException(
+				"Unable to create nested object entries");
+		}
+
+		List<T> list = new ArrayList<>();
+
+		for (Object object : (List<?>)item) {
+			validateOne(object);
+
+			list.add(parseOne(object));
+		}
+
+		return list;
+	}
+
+	protected abstract T parseOne(Object object);
+
+	protected void validateOne(Object object) {
+		if (!(object instanceof Map)) {
+			throw new BadRequestException(
+				"Unable to create nested object entries");
+		}
+	}
+
+	protected ObjectDefinition objectDefinition;
+
+}
---
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/ObjectEntryMtoMObjectRelationshipElementsParserImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.rest.internal.manager.v1_0;
+
+import com.liferay.object.constants.ObjectRelationshipConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectRelationship;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.internal.util.ObjectEntryValuesUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Carlos Correa
+ * @author Sergio Jimenez del Coso
+ */
+public class ObjectEntryMtoMObjectRelationshipElementsParserImpl
+	extends BaseObjectRelationshipElementsParserImpl<ObjectEntry> {
+
+	public ObjectEntryMtoMObjectRelationshipElementsParserImpl(
+		ObjectDefinition objectDefinition) {
+
+		super(objectDefinition);
+	}
+
+	@Override
+	public String getObjectRelationshipType() {
+		return ObjectRelationshipConstants.TYPE_MANY_TO_MANY;
+	}
+
+	@Override
+	public List<ObjectEntry> parse(
+			ObjectRelationship objectRelationship, Object value)
+		throws Exception {
+
+		return parseMany(value);
+	}
+
+	@Override
+	protected ObjectEntry parseOne(Object object) {
+		return ObjectEntryValuesUtil.toObjectEntry((Map<String, Object>)object);
+	}
+
+}
