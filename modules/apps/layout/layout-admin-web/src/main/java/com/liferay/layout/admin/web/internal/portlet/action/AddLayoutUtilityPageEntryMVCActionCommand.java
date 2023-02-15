a7601446c50ee5657ca9c912f19a5ce59bb05034
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-02-15 12:24:38 +0100

    LPS-174422 When creating a utility page check if we have a default structure and use it

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutUtilityPageEntryMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutUtilityPageEntryMVCActionCommand.java
@@ -16,7 +16,12 @@ package com.liferay.layout.admin.web.internal.portlet.action;
 
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.admin.web.internal.handler.LayoutUtilityPageEntryPortalExceptionRequestHandler;
+import com.liferay.layout.importer.LayoutsImporter;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
+import com.liferay.layout.utility.page.provider.LayoutUtilityPageEntryDefaultPageElementDefinitionProvider;
 import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -78,6 +83,14 @@ public class AddLayoutUtilityPageEntryMVCActionCommand
 					serviceContext.getScopeGroupId(), 0, 0, false, name, type,
 					masterLayoutPlid);
 
+			String pageElementJSON =
+				_layoutUtilityPageEntryDefaultPageElementDefinitionProvider.
+					getDefaultPageElementJSON(type);
+
+			if (pageElementJSON != null) {
+				_importPageElement(layoutUtilityPageEntry, pageElementJSON);
+			}
+
 			JSONObject jsonObject = JSONUtil.put(
 				"redirectURL",
 				_getRedirectURL(actionRequest, layoutUtilityPageEntry));
@@ -125,8 +138,42 @@ public class AddLayoutUtilityPageEntryMVCActionCommand
 			layoutFullURL, "p_l_mode", Constants.EDIT);
 	}
 
+	private void _importPageElement(
+			LayoutUtilityPageEntry layoutUtilityPageEntry,
+			String pageElementJSON)
+		throws Exception {
+
+		Layout layout = _layoutLocalService.getLayout(
+			layoutUtilityPageEntry.getPlid());
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					draftLayout.getGroupId(), draftLayout.getPlid(), true);
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+
+		_layoutsImporter.importPageElement(
+			draftLayout, layoutStructure, layoutStructure.getMainItemId(),
+			pageElementJSON, 0);
+	}
+
 	@Reference
 	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Reference
+	private LayoutsImporter _layoutsImporter;
+
+	@Reference
+	private LayoutUtilityPageEntryDefaultPageElementDefinitionProvider
+		_layoutUtilityPageEntryDefaultPageElementDefinitionProvider;
 
 	@Reference
 	private LayoutUtilityPageEntryLocalService
