7eb46fb4b4fdc7f6e42466b09152b5f55c54d33f
Amanda Albuquerque <amanda.albuquerque@liferay.com>
2023-02-15 14:58:51 -0300

    LPS-174836 Set status when amount is equal or higher than 15k

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFRequestForm/utils/submitForm.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFRequestForm/utils/submitForm.ts
@@ -24,6 +24,7 @@ import {ResourceName} from '../../../common/services/liferay/object/enum/resourc
 import createMDFRequest from '../../../common/services/liferay/object/mdf-requests/createMDFRequest';
 import updateMDFRequest from '../../../common/services/liferay/object/mdf-requests/updateMDFRequest';
 import {Status} from '../../../common/utils/constants/status';
+import getTotalMDFRequest from '../../../common/utils/getTotalMDFRequest';
 import createMDFRequestActivitiesProxyAPI from './createMDFRequestActivitiesProxyAPI';
 import createMDFRequestProxyAPI from './createMDFRequestProxyAPI';
 
@@ -37,6 +38,15 @@ export default async function submitForm(
 
 	if (currentRequestStatus) {
 		values.mdfRequestStatus = currentRequestStatus;
+	}
+
+	const totalMDFRequestAmount = getTotalMDFRequest(values.activities);
+
+	if (
+		totalMDFRequestAmount >= 15000 &&
+		values.mdfRequestStatus !== Status.DRAFT
+	) {
+		values.mdfRequestStatus = Status.MARKETING_DIRECTOR_REVIEW;
 	}
 
 	let dtoMDFRequest: mdfRequestDTO | undefined = undefined;
