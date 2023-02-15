a6ce6b76fa215e0861bf640a6e24944423ee9654
CalumR23 <searagan@gmail.com>
2023-02-15 10:39:54 -0800

    POSHI-465 Add check for unescaped quotes in list value

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/ForPoshiElement.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/ForPoshiElement.java
@@ -23,6 +23,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
+
 import org.dom4j.Attribute;
 import org.dom4j.Element;
 import org.dom4j.Node;
@@ -83,6 +85,16 @@ public class ForPoshiElement extends PoshiElement {
 
 			if (value.startsWith("\"") && value.endsWith("\"")) {
 				value = getDoubleQuotedContent(value);
+			}
+
+			if (value.contains("\"")) {
+				int escapedQuoteCount = StringUtils.countMatches(value, "\\\"");
+				int quoteCount = StringUtils.countMatches(value, "\"");
+
+				if (escapedQuoteCount != quoteCount) {
+					throw new RuntimeException(
+						"Unescaped quotes in list value: " + value);
+				}
 			}
 
 			addAttribute(matcher.group(2), value);
