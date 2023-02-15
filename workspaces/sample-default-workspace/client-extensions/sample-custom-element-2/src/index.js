7fc11ace729050f356440eeefd6a7b7020a4aea6
Gregory Amerson <gregory.amerson@liferay.com>
2023-02-15 17:20:23 -0600

    LPS-169846 sample-custom-element-2 oauth2 application user agent name

--- workspaces/sample-default-workspace/client-extensions/sample-custom-element-2/src/index.js
+++ workspaces/sample-default-workspace/client-extensions/sample-custom-element-2/src/index.js
@@ -37,7 +37,7 @@ class WebComponent extends HTMLElement {
 		super();
 
 		this.oAuth2Client = Liferay.OAuth2Client.FromUserAgentApplication(
-			'easy-oauth-application-user-agent'
+			'sample-oauth-application-user-agent'
 		);
 	}
 
