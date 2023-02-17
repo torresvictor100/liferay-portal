f7e0aab09848e8c9a3acd749194f147394b1106e
Alessio Rendina <asilcomando@gmail.com>
2023-02-17 17:34:08 +0100

    COMMERCE-9383 Added new option selector tag

---
+++ modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/OptionSelectorTag.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.frontend.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalServiceUtil;
+import com.liferay.info.item.renderer.InfoItemRenderer;
+import com.liferay.info.item.renderer.InfoItemRendererRegistry;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.taglib.servlet.PipingServletResponseFactory;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class OptionSelectorTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			InfoItemRenderer<CPDefinition> infoItemRenderer =
+				(InfoItemRenderer<CPDefinition>)
+					_infoItemRendererRegistry.getInfoItemRenderer(
+						"cpDefinition-option-selector");
+
+			infoItemRenderer.render(
+				CPDefinitionLocalServiceUtil.getCPDefinition(_cpDefinitionId),
+				(HttpServletRequest)pageContext.getRequest(),
+				PipingServletResponseFactory.createPipingServletResponse(
+					pageContext));
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			return SKIP_BODY;
+		}
+
+		return SKIP_BODY;
+	}
+
+	public long getCPDefinitionId() {
+		return _cpDefinitionId;
+	}
+
+	public String getNamespace() {
+		return _namespace;
+	}
+
+	public void setCPDefinitionId(long cpDefinitionId) {
+		_cpDefinitionId = cpDefinitionId;
+	}
+
+	public void setNamespace(String namespace) {
+		_namespace = namespace;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		setServletContext(ServletContextUtil.getServletContext());
+
+		_infoItemRendererRegistry =
+			ServletContextUtil.getInfoItemRendererRegistry();
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_cpDefinitionId = 0;
+		_infoItemRendererRegistry = null;
+		_namespace = StringPool.BLANK;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		OptionSelectorTag.class);
+
+	private long _cpDefinitionId;
+	private InfoItemRendererRegistry _infoItemRendererRegistry;
+	private String _namespace = StringPool.BLANK;
+
+}
