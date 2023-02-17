3240b995e5d4b4737539b9c32dfa351c67fe64b6
Victor Galan <victor.galan@liferay.com>
2023-02-17 10:24:59 +0100

    LPS-175951 Replace to the new class

--- modules/apps/upload/upload-test/src/testIntegration/java/com/liferay/upload/internal/test/DefaultUniqueFileNameProviderTest.java
+++ modules/apps/upload/upload-test/src/testIntegration/java/com/liferay/upload/internal/test/DefaultUniqueFileNameProviderTest.java
@@ -17,7 +17,7 @@ package com.liferay.upload.internal.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.upload.UploadServletRequestConfigurationHelperUtil;
+import com.liferay.portal.kernel.upload.configuration.UploadServletRequestConfigurationProviderUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.upload.UniqueFileNameProvider;
@@ -50,7 +50,7 @@ public class DefaultUniqueFileNameProviderTest {
 		String originalFileName = "filename.extension";
 
 		for (int i = 1;
-			 i <= UploadServletRequestConfigurationHelperUtil.getMaxTries();
+			 i <= UploadServletRequestConfigurationProviderUtil.getMaxTries();
 			 i++) {
 
 			String uniqueFileName = _uniqueFileNameProvider.provide(
@@ -78,7 +78,8 @@ public class DefaultUniqueFileNameProviderTest {
 		_uniqueFileNameProvider.provide(
 			"filename.extension",
 			_existsUntil(
-				UploadServletRequestConfigurationHelperUtil.getMaxSize() + 1));
+				UploadServletRequestConfigurationProviderUtil.getMaxSize() +
+					1));
 	}
 
 	@Test
