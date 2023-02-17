08f2a9f04cd60cfbc0463d74c4fcb62cc5ad98af
Brian Chan <brian.chan@liferay.com>
2023-02-17 11:14:11 -0300

    LPS-151626 Regen

--- workspaces/sample-minimal-workspace/client-extensions/sample-custom-element-2/src/routes/hello-world/pages/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelloWorld = () => (
-	<div className="hello-world">
-		<h1>Hello <span className="hello-world-name">World</span></h1>
-	</div>
-);
-
-export default HelloWorld;
