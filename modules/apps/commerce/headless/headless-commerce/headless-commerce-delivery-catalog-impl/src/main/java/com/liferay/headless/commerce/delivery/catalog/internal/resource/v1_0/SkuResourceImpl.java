613efe8c8422d9dff52309e24a7549af1a7fea3c
Alessio Rendina <asilcomando@gmail.com>
2023-02-17 17:23:33 +0100

    COMMERCE-9383 Added new endpoint implementation

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/resource/v1_0/SkuResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/resource/v1_0/SkuResourceImpl.java
@@ -20,6 +20,7 @@ import com.liferay.commerce.account.service.CommerceAccountLocalService;
 import com.liferay.commerce.account.util.CommerceAccountHelper;
 import com.liferay.commerce.context.CommerceContext;
 import com.liferay.commerce.context.CommerceContextFactory;
+import com.liferay.commerce.product.exception.NoSuchCPInstanceException;
 import com.liferay.commerce.product.exception.NoSuchCProductException;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
@@ -28,13 +29,20 @@ import com.liferay.commerce.product.permission.CommerceProductViewPermission;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.headless.commerce.delivery.catalog.dto.v1_0.DDMOption;
 import com.liferay.headless.commerce.delivery.catalog.dto.v1_0.Product;
 import com.liferay.headless.commerce.delivery.catalog.dto.v1_0.Sku;
 import com.liferay.headless.commerce.delivery.catalog.internal.dto.v1_0.converter.SkuDTOConverter;
 import com.liferay.headless.commerce.delivery.catalog.internal.dto.v1_0.converter.SkuDTOConverterContext;
 import com.liferay.headless.commerce.delivery.catalog.resource.v1_0.SkuResource;
 import com.liferay.portal.kernel.change.tracking.CTAware;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.vulcan.fields.NestedField;
 import com.liferay.portal.vulcan.fields.NestedFieldId;
@@ -51,6 +59,7 @@ import org.osgi.service.component.annotations.ServiceScope;
 
 /**
  * @author Andrea Sbarra
+ * @author Alessio Antonio Rendina
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/sku.properties",
@@ -125,6 +134,88 @@ public class SkuResourceImpl
 			pagination, totalItems);
 	}
 
+	@Override
+	public Sku postChannelProductSku(
+			Long channelId, Long productId, Long accountId, Integer quantity,
+			DDMOption[] ddmOptions)
+		throws Exception {
+
+		CPDefinition cpDefinition =
+			_cpDefinitionLocalService.fetchCPDefinitionByCProductId(productId);
+
+		if (cpDefinition == null) {
+			throw new NoSuchCProductException();
+		}
+
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.getCommerceChannel(channelId);
+
+		CommerceContext commerceContext = _getCommerceContext(
+			accountId, commerceChannel);
+
+		CommerceAccount commerceAccount = commerceContext.getCommerceAccount();
+
+		_commerceProductViewPermission.check(
+			PermissionCheckerFactoryUtil.create(contextUser),
+			commerceAccount.getCommerceAccountId(),
+			commerceChannel.getGroupId(), cpDefinition.getCPDefinitionId());
+
+		JSONArray jsonArray = JSONUtil.toJSONArray(
+			ddmOptions,
+			ddmOption -> _jsonFactory.createJSONObject(ddmOption.toString()));
+
+		CPInstance cpInstance = _cpInstanceHelper.fetchCPInstance(
+			cpDefinition.getCPDefinitionId(), JSONUtil.toString(jsonArray));
+
+		if (cpInstance == null) {
+			throw new NoSuchCPInstanceException();
+		}
+
+		return _skuDTOConverter.toDTO(
+			new SkuDTOConverterContext(
+				commerceContext, contextCompany.getCompanyId(), cpDefinition,
+				contextAcceptLanguage.getPreferredLocale(),
+				GetterUtil.getInteger(quantity, 1),
+				cpInstance.getCPInstanceId(), contextUriInfo, contextUser));
+	}
+
+	private CommerceContext _getCommerceContext(
+			Long accountId, CommerceChannel commerceChannel)
+		throws Exception {
+
+		int countUserCommerceAccounts =
+			_commerceAccountHelper.countUserCommerceAccounts(
+				contextUser.getUserId(), commerceChannel.getGroupId());
+
+		if (countUserCommerceAccounts > 1) {
+			if (accountId == null) {
+				throw new NoSuchAccountException();
+			}
+
+			return _commerceContextFactory.create(
+				contextCompany.getCompanyId(), commerceChannel.getGroupId(),
+				contextUser.getUserId(), 0, accountId);
+		}
+
+		long[] commerceAccountIds =
+			_commerceAccountHelper.getUserCommerceAccountIds(
+				contextUser.getUserId(), commerceChannel.getGroupId());
+
+		if (commerceAccountIds.length == 0) {
+			CommerceAccount commerceAccount =
+				_commerceAccountLocalService.getGuestCommerceAccount(
+					contextUser.getCompanyId());
+
+			commerceAccountIds = new long[] {
+				commerceAccount.getCommerceAccountId()
+			};
+		}
+
+		return _commerceContextFactory.create(
+			contextCompany.getCompanyId(), commerceChannel.getGroupId(),
+			contextUser.getUserId(), 0, commerceAccountIds[0]);
+	}
+
 	private List<Sku> _toSKUs(
 			Long channelId, Long accountId, List<CPInstance> cpInstances,
 			CPDefinition cpDefinition)
@@ -133,50 +224,17 @@ public class SkuResourceImpl
 		CommerceChannel commerceChannel =
 			_commerceChannelLocalService.getCommerceChannel(channelId);
 
-		int countUserCommerceAccounts =
-			_commerceAccountHelper.countUserCommerceAccounts(
-				contextUser.getUserId(), commerceChannel.getGroupId());
-
-		CommerceContext commerceContext;
-
-		if (countUserCommerceAccounts > 1) {
-			if (accountId == null) {
-				throw new NoSuchAccountException();
-			}
-
-			commerceContext = _commerceContextFactory.create(
-				contextCompany.getCompanyId(), commerceChannel.getGroupId(),
-				contextUser.getUserId(), 0, accountId);
-		}
-		else {
-			long[] commerceAccountIds =
-				_commerceAccountHelper.getUserCommerceAccountIds(
-					contextUser.getUserId(), commerceChannel.getGroupId());
-
-			if (commerceAccountIds.length == 0) {
-				CommerceAccount commerceAccount =
-					_commerceAccountLocalService.getGuestCommerceAccount(
-						contextUser.getCompanyId());
-
-				commerceAccountIds = new long[] {
-					commerceAccount.getCommerceAccountId()
-				};
-			}
-
-			commerceContext = _commerceContextFactory.create(
-				contextCompany.getCompanyId(), commerceChannel.getGroupId(),
-				contextUser.getUserId(), 0, commerceAccountIds[0]);
-		}
-
 		List<Sku> skus = new ArrayList<>();
 
 		for (CPInstance cpInstance : cpInstances) {
 			skus.add(
 				_skuDTOConverter.toDTO(
 					new SkuDTOConverterContext(
-						contextAcceptLanguage.getPreferredLocale(),
-						cpInstance.getCPInstanceId(), cpDefinition,
-						contextCompany.getCompanyId(), commerceContext)));
+						_getCommerceContext(accountId, commerceChannel),
+						contextCompany.getCompanyId(), cpDefinition,
+						contextAcceptLanguage.getPreferredLocale(), 1,
+						cpInstance.getCPInstanceId(), contextUriInfo,
+						contextUser)));
 		}
 
 		return skus;
@@ -201,7 +259,13 @@ public class SkuResourceImpl
 	private CPDefinitionLocalService _cpDefinitionLocalService;
 
 	@Reference
+	private CPInstanceHelper _cpInstanceHelper;
+
+	@Reference
 	private CPInstanceLocalService _cpInstanceLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private SkuDTOConverter _skuDTOConverter;
