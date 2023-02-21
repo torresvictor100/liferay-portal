b8cd329afa5c991bff685f76711e85e206710c62
Tang Hieu Ha <hhtank@yahoo.com>
2023-02-21 09:29:16 +0700

    LPS-166515 Deprecate class BackgroundTaskContextMapConstants in module level, add new class in portal-kernel *.constants

--- portal-kernel/src/com/liferay/portal/kernel/backgroundtask/constants/BackgroundTaskContextMapConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/backgroundtask/constants/BackgroundTaskContextMapConstants.java
@@ -12,14 +12,11 @@
  * details.
  */
 
-package com.liferay.portal.background.task.constants;
+package com.liferay.portal.kernel.backgroundtask.constants;
 
 /**
  * @author Michael C. Han
- * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
- * 				com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskContextMapConstants}
  */
-@Deprecated
 public class BackgroundTaskContextMapConstants {
 
 	public static final String DELETE_ON_SUCCESS = "deleteOnSuccess";
