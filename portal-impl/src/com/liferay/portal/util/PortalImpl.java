de4c241d9e48c9fd8dab43a3cb5cc3cbbe295561
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-02-21 15:37:59 +0100

    LPS-168385 Adds new reserved parameter for back url title into urls

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -479,6 +479,7 @@ public class PortalImpl implements Portal {
 		// Portal layout
 
 		_reservedParams.add("p_l_back_url");
+		_reservedParams.add("p_l_back_url_title");
 		_reservedParams.add("p_l_id");
 		_reservedParams.add("p_l_mode");
 		_reservedParams.add("p_l_reset");
