30bcd137506f96fb76eaf3ba99644654c07d28f0
Gabriel Henrique <gabriel.santos@liferay.com>
2023-02-23 14:36:48 -0300

    LPS-176561 Remove service from Staging module

--- modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/internal/portlet/DummyFolderPortlet.java
+++ modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/internal/portlet/DummyFolderPortlet.java
@@ -30,9 +30,10 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.preferences-unique-per-layout=false",
 		"javax.portlet.name=" + DummyFolderPortletKeys.DUMMY_FOLDER,
-		"javax.portlet.resource-bundle=content.Language"
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.version=3.0"
 	},
-	service = {DummyFolderPortlet.class, Portlet.class}
+	service = Portlet.class
 )
 public class DummyFolderPortlet extends MVCPortlet {
 
