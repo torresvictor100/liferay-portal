7308cd26198437625502ebd67d09757d2bf69070
Alan Huang <alan.huang@liferay.com>
2023-02-24 00:43:59 +0800

    LPS-174289 Auto SF

--- modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/service/test/OrganizationLocalServiceWhenSearchingOrganizationsTreeTest.java
+++ modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/service/test/OrganizationLocalServiceWhenSearchingOrganizationsTreeTest.java
@@ -50,11 +50,9 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.users.admin.kernel.util.UsersAdmin;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
 import org.junit.After;
 import org.junit.Assert;
