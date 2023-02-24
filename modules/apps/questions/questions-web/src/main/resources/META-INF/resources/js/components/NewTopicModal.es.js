2b1090eac640de2330b09808f918cbb148cd75fa
João Victor Torres Araujo <torresvictor100@gmail.com>
2023-02-24 13:57:07 -0300

    disable unsubscribe button in new topic

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/NewTopicModal.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/NewTopicModal.es.js
@@ -150,7 +150,7 @@ export default function NewTopicModal({
 							<ClayButton.Group spaced>
 								<ClayButton
 									displayType="secondary"
-									onClick={close}
+									
 								>
 									{Liferay.Language.get('cancel')}
 								</ClayButton>
