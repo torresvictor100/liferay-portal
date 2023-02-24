cc7b81b81b91dbeb0582088ddf322e3c9ba9542b
Raymond Augé <raymond.auge@liferay.com>
2023-02-24 16:57:26 -0500

    LPS-174373 batch-engine - INTEGRATION TEST - runs in OSGi so use bundle API to build zip

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineBundleTrackerTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineBundleTrackerTest.java
@@ -17,15 +17,20 @@ package com.liferay.batch.engine.internal.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.batch.engine.unit.BatchEngineUnit;
 import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
-import com.liferay.batch.engine.zip.BatchEngineZipUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.zip.ZipWriter;
+import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.io.File;
 import java.io.FileInputStream;
 
+import java.net.URL;
+
+import java.util.Enumeration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -57,22 +62,21 @@ public class BatchEngineBundleTrackerTest {
 
 	@Before
 	public void setUp() throws Exception {
-		Bundle bundle = FrameworkUtil.getBundle(
-			BatchEngineBundleTrackerTest.class);
+		_bundle = FrameworkUtil.getBundle(BatchEngineBundleTrackerTest.class);
 
-		_bundleContext = bundle.getBundleContext();
+		_bundleContext = _bundle.getBundleContext();
 	}
 
 	@Test
 	public void testBatchEngineBundles() throws Exception {
-		_testBatchEngineBundle("dependencies/batch1", 1);
-		_testBatchEngineBundle("dependencies/batch2", 0);
-		_testBatchEngineBundle("dependencies/batch3", 2);
-		_testBatchEngineBundle("dependencies/batch4", 3);
-		_testBatchEngineBundle("dependencies/batch5", 1);
-		_testBatchEngineBundle("dependencies/batch6", 2);
-		_testBatchEngineBundle("dependencies/batch7", 1);
-		_testBatchEngineBundle("dependencies/batch8", 3);
+		_testBatchEngineBundle("batch1/", 1);
+		_testBatchEngineBundle("batch2/", 0);
+		_testBatchEngineBundle("batch3/", 2);
+		_testBatchEngineBundle("batch4/", 3);
+		_testBatchEngineBundle("batch5/", 1);
+		_testBatchEngineBundle("batch6/", 2);
+		_testBatchEngineBundle("batch7/", 1);
+		_testBatchEngineBundle("batch8/", 3);
 	}
 
 	private void _testBatchEngineBundle(
@@ -103,8 +107,7 @@ public class BatchEngineBundleTrackerTest {
 
 		String bundleSymbolicName = RandomTestUtil.randomString();
 
-		File zipFile = BatchEngineZipUtil.toJarFile(
-			BatchEngineBundleTrackerTest.class, batchFile);
+		File zipFile = _toJarFile(_PATH_DEPENDENCIES.concat(batchFile));
 
 		Bundle bundle = _bundleContext.installBundle(
 			bundleSymbolicName, new FileInputStream(zipFile));
@@ -123,6 +126,38 @@ public class BatchEngineBundleTrackerTest {
 		}
 	}
 
+	private File _toJarFile(String basePath) throws Exception {
+		ZipWriter zipWriter = ZipWriterFactoryUtil.getZipWriter();
+
+		Enumeration<URL> enumeration = _bundle.findEntries(basePath, "*", true);
+
+		if (enumeration != null) {
+			while (enumeration.hasMoreElements()) {
+				URL url = enumeration.nextElement();
+
+				String path = url.getPath();
+
+				if (path.endsWith(StringPool.SLASH)) {
+					continue;
+				}
+
+				String zipPath = path.substring(basePath.length());
+
+				if (zipPath.startsWith(StringPool.SLASH)) {
+					zipPath = zipPath.substring(1);
+				}
+
+				zipWriter.addEntry(zipPath, url.openStream());
+			}
+		}
+
+		return zipWriter.getFile();
+	}
+
+	private static final String _PATH_DEPENDENCIES =
+		"com/liferay/batch/engine/internal/test/dependencies/";
+
+	private Bundle _bundle;
 	private BundleContext _bundleContext;
 
 	private class TestBatchEngineUnitProcessor
