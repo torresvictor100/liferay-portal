2adbdc0476c37ac03f179d1e12c9e7f1244be60b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-02-27 08:04:10 +0000

    LPS-176786 Moves classes to site-api

--- modules/apps/site/site-api/src/main/java/com/liferay/site/util/SitemapURLProvider.java
+++ modules/apps/site/site-api/src/main/java/com/liferay/site/util/SitemapURLProvider.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.layout.admin.kernel.util;
+package com.liferay.site.util;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.LayoutSet;
