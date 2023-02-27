f268a2c703f8e6bc4141f65503e9b2b1c60507a1
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-02-27 09:00:58 +0100

    LPS-176776 Hide behind FF

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/initializer/BlankSiteInitializer.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/initializer/BlankSiteInitializer.java
@@ -23,6 +23,7 @@ import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
 import com.liferay.layout.utility.page.provider.LayoutUtilityPageEntryDefaultPageElementDefinitionProvider;
 import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryService;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -72,9 +73,11 @@ public class BlankSiteInitializer implements SiteInitializer {
 
 	@Override
 	public void initialize(long groupId) throws InitializationException {
-		_addLayoutUtilityPageEntry(
-			404, groupId, "404 Error",
-			LayoutUtilityPageEntryConstants.TYPE_SC_NOT_FOUND);
+		if (FeatureFlagManagerUtil.isEnabled("LPS-162765")) {
+			_addLayoutUtilityPageEntry(
+				404, groupId, "404 Error",
+				LayoutUtilityPageEntryConstants.TYPE_SC_NOT_FOUND);
+		}
 	}
 
 	@Override
