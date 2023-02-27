00b0b80f849a1b7e4fd0cd62642d6264cc54cad9
Lily.Chi <lily.chi@liferay.com>
2023-02-27 16:09:13 +0800

    LPS-176129 Remove stream usage in method body from ServiceXMLTest

--- portal-impl/test/unit/com/liferay/portal/service/ServiceXMLTest.java
+++ portal-impl/test/unit/com/liferay/portal/service/ServiceXMLTest.java
@@ -14,18 +14,22 @@
 
 package com.liferay.portal.service;
 
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.Objects;
-import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -44,29 +48,47 @@ public class ServiceXMLTest {
 
 	@Test
 	public void testTXRequired() throws Exception {
-		Stream<Path> stream = Files.find(
-			Paths.get(System.getProperty("user.dir")), Integer.MAX_VALUE,
-			ServiceXMLTest::_isServiceXml, FileVisitOption.FOLLOW_LINKS);
+		Path portalPath = Paths.get(System.getProperty("user.dir"));
 
-		stream.forEach(ServiceXMLTest::_assertNoTXRequiredElement);
+		Files.walkFileTree(
+			portalPath,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+						Path path, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					if (_isServiceXml(path)) {
+						_assertNoTXRequiredElement(path.toFile());
+
+						return FileVisitResult.SKIP_SIBLINGS;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
 	}
 
-	private static void _assertNoTXRequiredElement(Path path) {
-		try {
-			Stream<String> stream = Files.lines(path);
+	private static void _assertNoTXRequiredElement(File file) throws IOException {
+		try (FileInputStream fileInputStream = new FileInputStream(file);
+			UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(
+					new InputStreamReader(fileInputStream))) {
 
-			Assert.assertFalse(
-				"Remove deprecated tx-required element from " + path,
-				stream.anyMatch(line -> line.contains("<tx-required>")));
-		}
-		catch (IOException ioException) {
-			throw new RuntimeException(ioException);
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				Assert.assertFalse(
+					"Remove deprecated tx-required element from " +
+						file.getPath(),
+					line.contains("<tx-required>"));
+			}
 		}
 	}
 
-	private static boolean _isServiceXml(
-		Path path, BasicFileAttributes basicFileAttributes) {
-
+	private static boolean _isServiceXml(Path path) {
 		Path fileNamePath = path.getFileName();
 
 		if (Objects.equals(fileNamePath.toString(), "service.xml")) {
