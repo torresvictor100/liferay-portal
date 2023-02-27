7a7f1472675456f4712ca3a08d949657de7eeb3e
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-02-27 10:51:34 -0800

    LRCI-3346 Add Description & Descriptor classes to recall configured data from the database

--- modules/test/jenkins-plugin-events/src/main/java/com/liferay/jenkins/plugin/events/JenkinsEventsRootAction.java
+++ modules/test/jenkins-plugin-events/src/main/java/com/liferay/jenkins/plugin/events/JenkinsEventsRootAction.java
@@ -16,13 +16,55 @@ package com.liferay.jenkins.plugin.events;
 
 import hudson.Extension;
 
+import hudson.model.Describable;
+import hudson.model.Descriptor;
 import hudson.model.RootAction;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import jenkins.model.Jenkins;
+
+import org.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * @author Michael Hashimoto
  */
 @Extension
-public class JenkinsEventsRootAction implements RootAction {
+public class JenkinsEventsRootAction
+	extends Descriptor<JenkinsEventsRootAction>
+	implements Describable<JenkinsEventsRootAction>, RootAction {
+
+	public JenkinsEventsRootAction() {
+		super(JenkinsEventsRootAction.class);
+
+		load();
+	}
+
+	public void doJenkinsEventsConfiguration(
+			StaplerRequest staplerRequest, StaplerResponse staplerResponse)
+		throws IOException, ServletException {
+
+		JSONObject jsonObject = new JSONObject(
+			staplerRequest.getParameter("json"));
+
+		webHookURL = jsonObject.getString("webHookURL");
+
+		save();
+
+		Jenkins jenkins = Jenkins.getActiveInstance();
+
+		staplerResponse.sendRedirect(jenkins.getRootUrl());
+	}
+
+	@Override
+	public Descriptor<JenkinsEventsRootAction> getDescriptor() {
+		return this;
+	}
 
 	@Override
 	public String getDisplayName() {
@@ -38,5 +80,7 @@ public class JenkinsEventsRootAction implements RootAction {
 	public String getUrlName() {
 		return "jenkins-events";
 	}
+
+	public String webHookURL;
 
 }
