a1344e75134bd54e7c960be80410fbd2fff6b484
Jonathan McCann <jonathan.mccann@liferay.com>
2023-03-02 11:42:32 -0800

    LPS-157194 Add upgrade process for 7.3 to clear out expired OAuth2Authorizations
    In 7.3 and above this is done via a scheduled job. However, upgrading from 7.2, there may be millions of entries which can cause server instability

---
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/internal/upgrade/v3_0_1/OAuth2AuthorizationUpgradeProcess.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.internal.upgrade.v3_0_1;
+
+import com.liferay.oauth2.provider.configuration.OAuth2ProviderConfiguration;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.Time;
+
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+
+/**
+ * @author Jonathan McCann
+ */
+public class OAuth2AuthorizationUpgradeProcess extends UpgradeProcess {
+
+	public OAuth2AuthorizationUpgradeProcess(
+		ConfigurationProvider configurationProvider) {
+
+		_configurationProvider = configurationProvider;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		OAuth2ProviderConfiguration oAuth2ProviderConfiguration =
+			_configurationProvider.getSystemConfiguration(
+				OAuth2ProviderConfiguration.class);
+
+		int expiredAuthorizationsAfterlifeDuration = Math.max(
+			oAuth2ProviderConfiguration.
+				expiredAuthorizationsAfterlifeDuration(),
+			0);
+
+		long expiredAuthorizationsAfterlifeDurationMillis =
+			expiredAuthorizationsAfterlifeDuration * Time.SECOND;
+
+		Timestamp purgeDate = new Timestamp(System.currentTimeMillis());
+
+		purgeDate.setTime(
+			purgeDate.getTime() - expiredAuthorizationsAfterlifeDurationMillis);
+
+		String sql = StringBundler.concat(
+			"create table TEMP_TABLE as (",
+			"select * from OAuth2Authorization where ",
+			"(accessTokenExpirationDate >= ?) or ((refreshTokenExpirationDate ",
+			"is not null) and (refreshTokenExpirationDate >= ?)))");
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				sql)) {
+
+			preparedStatement.setTimestamp(1, purgeDate);
+			preparedStatement.setTimestamp(2, purgeDate);
+
+			preparedStatement.execute();
+		}
+
+		runSQL(
+			StringBundler.concat(
+				"create table TEMP_TABLE2 as (",
+				"select * from OA2Auths_OA2ScopeGrants where ",
+				"oAuth2AuthorizationId in (select oAuth2AuthorizationId from ",
+				"TEMP_TABLE))"));
+
+		runSQL("drop table OAuth2Authorization");
+		runSQL("drop table OA2Auths_OA2ScopeGrants");
+
+		runSQL("rename table TEMP_TABLE to OAuth2Authorization");
+		runSQL("rename table TEMP_TABLE2 to OA2Auths_OA2ScopeGrants");
+	}
+
+	private final ConfigurationProvider _configurationProvider;
+
+}
