f47ced047896634ef366d9db88517c9aab14c876
Kenji Heigel <kenji.heigel@liferay.com>
2023-03-03 17:01:05 -0800

    POSHI-501 Sort and expose stored exceptions

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/script/PoshiScriptParserException.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/script/PoshiScriptParserException.java
@@ -17,13 +17,14 @@ package com.liferay.poshi.core.script;
 import com.liferay.poshi.core.elements.PoshiElement;
 import com.liferay.poshi.core.elements.PoshiElementException;
 import com.liferay.poshi.core.elements.PoshiNode;
+import com.liferay.poshi.core.util.NaturalOrderStringComparator;
 import com.liferay.poshi.core.util.StringUtil;
 
 import java.net.URL;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 /**
  * @author Kenji Heigel
@@ -37,17 +38,54 @@ public class PoshiScriptParserException extends PoshiElementException {
 		_poshiScriptParserExceptions.clear();
 	}
 
+	public static List<PoshiScriptParserException> getExceptions() {
+		Collections.sort(
+			_poshiScriptParserExceptions,
+			(poshiScriptParserException1, poshiScriptParserException2) -> {
+				String filePath1 = poshiScriptParserException1.getFilePath();
+				String filePath2 = poshiScriptParserException2.getFilePath();
+
+				NaturalOrderStringComparator naturalOrderStringComparator =
+					new NaturalOrderStringComparator();
+
+				if (!filePath1.equals(filePath2)) {
+					return naturalOrderStringComparator.compare(
+						filePath1, filePath2);
+				}
+
+				return naturalOrderStringComparator.compare(
+					String.valueOf(
+						poshiScriptParserException1.getErrorLineNumber()),
+					String.valueOf(
+						poshiScriptParserException2.getErrorLineNumber()));
+			});
+
+		return _poshiScriptParserExceptions;
+	}
+
 	public static void throwExceptions() throws Exception {
 		if (!_poshiScriptParserExceptions.isEmpty()) {
 			StringBuilder sb = new StringBuilder();
 
 			sb.append("\n\n");
 			sb.append(_poshiScriptParserExceptions.size());
-			sb.append(" errors in Poshi script syntax\n\n");
+			sb.append(" error");
+
+			if (_poshiScriptParserExceptions.size() > 1) {
+				sb.append("s");
+			}
+
+			sb.append(" in Poshi Script syntax\n\n");
+
+			int i = 1;
 
 			for (Exception exception : _poshiScriptParserExceptions) {
+				sb.append(i);
+				sb.append(". ");
 				sb.append(exception.getMessage());
 				sb.append("\n\n");
+
+				i++;
 			}
 
 			System.out.println(sb.toString());
@@ -57,12 +95,8 @@ public class PoshiScriptParserException extends PoshiElementException {
 	}
 
 	public static void throwExceptions(String filePath) throws Exception {
-		if (_poshiScriptParserExceptions.isEmpty()) {
-			return;
-		}
-
 		for (PoshiScriptParserException poshiScriptParserException :
-				_poshiScriptParserExceptions) {
+				getExceptions()) {
 
 			if (!filePath.equals(poshiScriptParserException.getFilePath())) {
 				continue;
@@ -128,8 +162,8 @@ public class PoshiScriptParserException extends PoshiElementException {
 			StringUtil.count(parentPoshiScript, "\n", index);
 	}
 
-	private static final Set<PoshiScriptParserException>
-		_poshiScriptParserExceptions = Collections.synchronizedSet(
-			new HashSet<>());
+	private static final List<PoshiScriptParserException>
+		_poshiScriptParserExceptions = Collections.synchronizedList(
+			new ArrayList<>());
 
 }
