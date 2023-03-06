56984a184f14bf2db0129f5d0960dcd507691ee1
Alan Huang <alan.huang@liferay.com>
2023-03-06 21:28:25 +0800

    LPS-176090 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaModuleUniqueVerifyProcessCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaModuleUniqueVerifyProcessCheck.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * @author Alan
+ * @author Alan Huang
  */
 public class JavaModuleUniqueVerifyProcessCheck extends BaseJavaTermCheck {
 
