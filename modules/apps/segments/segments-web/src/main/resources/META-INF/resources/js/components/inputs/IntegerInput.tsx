4a3b0094f5e8580a8c61eb3dcea1842b613e4cbf
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2023-03-06 19:40:54 +0100

    LPS-176077 Migrate IntegerInput component to TypeScript

---
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/inputs/IntegerInput.tsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClaySelectWithOption} from '@clayui/form';
+import React, {ChangeEvent} from 'react';
+
+interface Props {
+	disabled?: boolean;
+	onChange: (payload: {value: string}) => void;
+	options: Array<{
+		disabled: boolean;
+		label: string;
+		value: string;
+	}>;
+
+	value?: number | string;
+}
+
+function IntegerInput({disabled, onChange, options, value}: Props) {
+	const handleIntegerChange = (
+		event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+	) => {
+		const value = parseInt(event.target.value, 10);
+
+		if (!isNaN(value)) {
+			onChange({
+				value: value.toString(),
+			});
+		}
+	};
+
+	return options?.length ? (
+		<ClaySelectWithOption
+			className="criterion-input form-control"
+			data-testid="options-integer"
+			disabled={disabled}
+			onChange={handleIntegerChange}
+			options={options}
+			value={value}
+		/>
+	) : (
+		<input
+			className="criterion-input form-control"
+			data-testid="integer-number"
+			disabled={disabled}
+			onChange={handleIntegerChange}
+			type="number"
+			value={value}
+		/>
+	);
+}
+
+export default IntegerInput;
---
+++ modules/apps/segments/segments-web/types/src/main/resources/META-INF/resources/js/components/inputs/IntegerInput.d.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/// <reference types="react" />
+
+interface Props {
+	disabled?: boolean;
+	onChange: (payload: {value: string}) => void;
+	options: Array<{
+		disabled: boolean;
+		label: string;
+		value: string;
+	}>;
+	value?: number | string;
+}
+declare function IntegerInput({
+	disabled,
+	onChange,
+	options,
+	value,
+}: Props): JSX.Element;
+export default IntegerInput;
