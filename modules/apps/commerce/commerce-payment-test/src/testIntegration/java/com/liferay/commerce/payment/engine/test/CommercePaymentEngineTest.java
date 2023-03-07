89fcce08322429323467ba2427ab8e776a654879
Brian Chan <brian.chan@liferay.com>
2023-03-07 10:45:33 -0300

    COMMERCE-10944 Rename

--- modules/apps/commerce/commerce-payment-test/src/testIntegration/java/com/liferay/commerce/payment/engine/test/CommercePaymentEngineTest.java
+++ modules/apps/commerce/commerce-payment-test/src/testIntegration/java/com/liferay/commerce/payment/engine/test/CommercePaymentEngineTest.java
@@ -107,7 +107,7 @@ public class CommercePaymentEngineTest {
 		_commerceChannel = CommerceTestUtil.addCommerceChannel(
 			_group.getGroupId(), _commerceCurrency.getCode());
 
-		_originalGroup = CommerceGroupThreadLocal.get();
+		_originalCommerceGroup = CommerceGroupThreadLocal.get();
 
 		CommerceGroupThreadLocal.set(
 			_groupLocalService.fetchGroup(_commerceChannel.getGroupId()));
@@ -126,7 +126,7 @@ public class CommercePaymentEngineTest {
 
 	@After
 	public void tearDown() throws Exception {
-		CommerceGroupThreadLocal.set(_originalGroup);
+		CommerceGroupThreadLocal.set(_originalCommerceGroup);
 
 		for (CommerceOrder commerceOrder : _commerceOrders) {
 			_commerceOrderLocalService.deleteCommerceOrder(commerceOrder);
@@ -342,7 +342,7 @@ public class CommercePaymentEngineTest {
 	private GroupLocalService _groupLocalService;
 
 	private HttpServletRequest _httpServletRequest;
-	private Group _originalGroup;
+	private Group _originalCommerceGroup;
 	private ServiceContext _serviceContext;
 
 }
