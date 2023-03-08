14e39aaccb0c407b320999d3e9bb894f54b85856
Brian Chan <brian.chan@liferay.com>
2023-03-08 08:55:20 -0300

    COMMERCE-10759 0 will not work in every DB

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v8_9_3/CommerceCountryUpgradeProcess.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v8_9_3/CommerceCountryUpgradeProcess.java
@@ -36,7 +36,7 @@ public class CommerceCountryUpgradeProcess extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		try (Statement selectStatement = connection.createStatement()) {
 			ResultSet resultSet = selectStatement.executeQuery(
-				"select * from Country where groupFilterEnabled = " + 0);
+				"select * from Country where groupFilterEnabled = [$FALSE$]");
 
 			while (resultSet.next()) {
 				long countryId = resultSet.getLong("countryId");
