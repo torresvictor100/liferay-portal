fb5dae4e7e06d0471f466d3762ce31a7ed393f14
Igor Franca <igor.franca@liferay.com>
2023-03-09 17:28:05 -0300

    LPS-177853 Create next step page

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/NextStepPage/NextStepPage.tsx
@@ -0,0 +1,76 @@
+import ClayIcon from '@clayui/icon';
+
+import {AccountAndAppCard} from '../../components/Card/AccountAndAppCard';
+import {Footer} from '../../components/Footer/Footer';
+import {Header} from '../../components/Header/Header';
+import {NewAppPageFooterButtons} from '../../components/NewAppPageFooterButtons/NewAppPageFooterButtons';
+
+import './NextStepPage.scss';
+interface NextStepPageProps {
+	accountLogo: string;
+	accountName: string;
+	appCategory: string;
+	appLogo: string;
+	appName: string;
+	orderId: number;
+}
+
+export function NextStepPage({
+	accountLogo,
+	accountName,
+	appCategory,
+	appLogo,
+	appName,
+	orderId,
+}: NextStepPageProps) {
+	return (
+		<>
+			<div className="next-step-page-container">
+				<div className="next-step-page-content">
+					<div className="next-step-page-cards">
+						<AccountAndAppCard
+							category={appCategory}
+							logo={appLogo}
+							title={appName}
+						></AccountAndAppCard>
+
+						<ClayIcon
+							className="next-step-page-icon"
+							symbol="arrow-right-full"
+						/>
+
+						<AccountAndAppCard
+							category="Console"
+							logo={accountLogo}
+							title={accountName}
+						></AccountAndAppCard>
+					</div>
+
+					<div className="next-step-page-text">
+						<Header
+							description={`Congratulations on the purchase of ${appName}. You will now need to configure the app in the Cloud Console. To access the Cloud Console, click the button below and provide your Order ID when prompted.`}
+							title="Next steps"
+						/>
+
+						<span>
+							Your Order ID is: <strong>{orderId}</strong>
+						</span>
+					</div>
+
+					<NewAppPageFooterButtons
+						backButtonText="Go back to  Dashboard"
+						continueButtonText="Continue Configuration"
+						onClickBack={() => {}}
+						onClickContinue={() => {}}
+					/>
+
+					<div className="next-step-page-link">
+						<a>Learn more about App configuration</a>
+					</div>
+				</div>
+
+				<Footer />
+			</div>
+		</>
+	);
+}
