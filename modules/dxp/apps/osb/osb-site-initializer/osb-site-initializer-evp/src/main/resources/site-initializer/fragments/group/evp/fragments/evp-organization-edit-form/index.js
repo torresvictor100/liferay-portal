a33a97d685a21fe194f6936432fdb2bed756efa3
Claudionilton <claudionilton.junior@liferay.com>
2023-03-09 11:12:30 -0300

    LPS-172953 Create Edit Finance Form Modify Kebab

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-organization-edit-form/index.js
@@ -0,0 +1,159 @@
+/* eslint-disable @liferay/portal/no-global-fetch */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const queryString = window.location.search;
+const urlParams = queryString.split('=');
+const organizationId = urlParams[1];
+const liferayUrl = window.location.origin;
+
+const searchParams = new URLSearchParams();
+
+searchParams.set('filter', `id eq '${organizationId}'`);
+searchParams.set(
+	'fields',
+	'bankingInfo,city,contactEmail,contactName,contactPhone,country,id,organizationName,organizationSiteSocialMediaLink,smallDescription,state,street,taxId,taxIdentificationNumber,zip'
+);
+
+function getOrganizationFormValues() {
+	const evpOrganizationForm = document.querySelector(
+		'.evp-organization-form'
+	);
+
+	if (!evpOrganizationForm) {
+		return console.error('Evp Form not found');
+	}
+
+	const organizationForm = {};
+	const formData = new FormData(evpOrganizationForm);
+
+	for (const [key, value] of Array.from(formData.entries())) {
+		if (!ignoreFields.includes(key)) {
+			organizationForm[key] = value;
+		}
+	}
+
+	return organizationForm;
+}
+
+async function getEVPOrganizations() {
+	const response = await fetch(
+		`${liferayUrl}/o/c/evporganizations?${searchParams.toString()}`,
+		{
+			headers: {
+				'Content-type': 'application/json',
+				'x-csrf-token': Liferay.authToken,
+			},
+		}
+	);
+
+	const data = await response.json();
+
+	return data?.items ?? [];
+}
+
+getEVPOrganizations().then((organizations) => {
+	for (const organization of organizations) {
+		for (const key in organization) {
+			const inputName = document.querySelector(`[name='${key}']`);
+
+			if (inputName) {
+				inputName.value = organization[key];
+			}
+		}
+	}
+});
+
+const ignoreFields = [
+	'classTypeId',
+	'classNameId',
+	'formItemId',
+	'p_l_mode',
+	'plid',
+	'redirect',
+	'organizationStatus',
+	'organizationStatus-label',
+];
+
+const organizationUpdate = async () => {
+	const organizationForm = getOrganizationFormValues();
+
+	organizationForm.organizationStatus = {
+		key: 'verified',
+		name: 'Verified',
+	};
+
+	await fetch(`${liferayUrl}/o/c/evporganizations/${organizationId}`, {
+		body: JSON.stringify(organizationForm),
+		headers: {
+			'content-type': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+		method: 'PUT',
+	});
+};
+
+const formInputName = document.querySelector('.submit-button');
+const toolTip = document.querySelector('.tooltip');
+
+function changeDisplayTooltip(display) {
+	toolTip.style.display = display;
+}
+
+changeDisplayTooltip('none');
+
+if (!formInputName) {
+	return;
+}
+
+formInputName.addEventListener('click', (event) => {
+	const inputValues = [];
+	const organizationForm = getOrganizationFormValues();
+
+	let error = 0;
+
+	const formInputs = Object.entries(organizationForm);
+
+	inputValues.push(formInputs);
+
+	const firstInputValue = inputValues[0]?.find((item) => item[1] === '');
+	if (firstInputValue !== undefined) {
+		const inputName = document.querySelector(
+			`[name='${firstInputValue[0]}']`
+		);
+		const div = document.createElement('div');
+
+		div.setAttribute('class', firstInputValue[0]);
+
+		changeDisplayTooltip('none');
+
+		if (!firstInputValue[1]) {
+			const inputParentDiv = inputName.parentNode;
+			const divElement = document.createElement('div');
+
+			divElement.appendChild(toolTip);
+			inputParentDiv.appendChild(divElement);
+
+			changeDisplayTooltip('block');
+
+			inputName.addEventListener('load', () =>
+				changeDisplayTooltip('block')
+			);
+			event.preventDefault();
+
+			error++;
+		}
+	}
+
+	if (error === 0) {
+		organizationUpdate();
+	}
+});
