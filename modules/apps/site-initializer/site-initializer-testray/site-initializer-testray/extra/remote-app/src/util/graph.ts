e5649923cd015a37bdf8acbc78fca651f991468c
Keven Leone <keven.leone@liferay.com>
2023-03-10 11:12:51 -0300

    LPS-178177 Update Chart Style to use BEM
    Co-authored-by: Álvaro Leal <alvaro.leal@liferay.com>
    Co-authored-by: Samuel Lopes <samuel.lopes@liferay.com>
    Co-authored-by: Rafael Ferreira <rafael.ferreira@liferay.com>
    Co-authored-by: Wellington dos Santos Barbosa <wellington.barbosa@liferay.com>
    Co-authored-by: Elvison.Victor <elvison.victor@liferay.com>

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/util/graph.ts
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/util/graph.ts
@@ -17,25 +17,22 @@ import * as d3Instance from 'd3';
 import {DATA_COLORS} from './constants';
 import {findAndReplaceProperty, getPercentLabel} from './graph.util';
 
-export const TPL_LEGEND_ITEM = `
- <div class="legend-color-block" style="background-color: {color}"></div>
- <div class="legend-item-key">
-     <div class="legend-item-value">
-         <div class="legend-item-numbers">
-             <span class="primary">{value}</span>
-             <span class="ml-1">({percent})</span>
-         </div>
-
-         <div class="legend-item-label">{label}</div>
-     </div>
-</div>
- `;
-
 interface DonutLegendOptions {
 	data: any[];
 	elementId: string;
 	total: number;
 }
+
+export const TPL_LEGEND_ITEM = `
+<div class="tr-legend-container__item__square" style="background-color: {color}"></div>
+<div class="tr-legend-container__item__metrics">
+	<div class="tr-legend-container__item__metrics__metric">
+		<span class="tr-legend-container__item__metrics__metric--value">{value}</span>
+		<span class="tr-legend-container__item__metrics__metric--percent">({percent})</span>
+	</div>
+	<div class="tr-legend-container__item__metrics__label">{label}</div>
+</div>
+ `;
 
 export function getDonutLegend(graph: any, options: DonutLegendOptions) {
 	const {data, elementId, total} = options;
@@ -49,13 +46,13 @@ export function getDonutLegend(graph: any, options: DonutLegendOptions) {
 	d3Instance
 		.select('#' + elementId)
 		.insert('ul', '#' + elementId)
-		.attr('class', 'legend-container')
+		.attr('class', 'tr-legend-container')
 		.selectAll('li')
 		.data(data)
 		.enter()
 		.append('li')
-		.attr('class', 'legend-item')
-		.html((id: any) => {
+		.attr('class', 'tr-legend-container__item')
+		.html((id) => {
 			const [value] = graph.data.values(id);
 
 			return findAndReplaceProperty(TPL_LEGEND_ITEM, {
@@ -67,6 +64,6 @@ export function getDonutLegend(graph: any, options: DonutLegendOptions) {
 				value,
 			});
 		})
-		.on('mouseover', (id: string) => graph.focus(id.toUpperCase()))
+		.on('mouseover', (id) => graph.focus(id.toUpperCase()))
 		.on('mouseout', () => graph.revert());
 }
