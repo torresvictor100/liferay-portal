592740f3864dd010c569d95b6b2ab078e63fe383
Alan Huang <alan.huang@liferay.com>
2023-03-11 16:06:12 +0800

    LPS-178042 Catch more cases

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONValidationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONValidationCheck.java
@@ -17,6 +17,9 @@ package com.liferay.source.formatter.check;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.tools.ToolsUtil;
+
+import java.io.IOException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +32,8 @@ public class JSONValidationCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws IOException {
 
 		if (Validator.isNull(content)) {
 			return content;
@@ -54,17 +58,45 @@ public class JSONValidationCheck extends BaseFileCheck {
 		return content;
 	}
 
-	private String _removeJSONComments(String content) {
-		content = content.replaceAll("\\/\\*\\s+.+\\s+\\*\\/", "");
+	private String _removeJSONComments(String content) throws IOException {
+		int x = -1;
 
-		String[] lines = content.split("\\r?\\n");
+		while (true) {
+			x = content.indexOf("/*", x + 1);
 
-		for (String line : lines) {
-			String trimedLine = line.trim();
-
-			if (trimedLine.startsWith("//")) {
-				content = StringUtil.removeSubstring(content, line);
+			if (x == -1) {
+				break;
 			}
+
+			if (ToolsUtil.isInsideQuotes(content, x)) {
+				continue;
+			}
+
+			int y = content.indexOf("*/", x + 2);
+
+			return content.substring(0, x) + content.substring(y + 2);
+		}
+
+		x = -1;
+
+		while (true) {
+			x = content.indexOf("//", x + 1);
+
+			if (x == -1) {
+				break;
+			}
+
+			if (ToolsUtil.isInsideQuotes(content, x)) {
+				continue;
+			}
+
+			int y = content.indexOf("\n", x);
+
+			if (y != -1) {
+				return content.substring(0, x) + content.substring(y);
+			}
+
+			return content.substring(0, x);
 		}
 
 		return content;
