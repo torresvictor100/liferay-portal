b4d0fbc2f7c78732c265d9e64abf7f5bbd8f03d1
Peter Yoo <peter.yoo@liferay.com>
2023-03-14 08:22:41 -0700

    LRCI-3454 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BuildDatabaseUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BuildDatabaseUtil.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
