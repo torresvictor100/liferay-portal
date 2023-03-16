df502384480c19fd89e06e26b5e2007b679dee21
Brian Chan <brian.chan@liferay.com>
2023-03-16 15:32:18 -0300

    LPS-172471 Rename

--- workspaces/refresh_other_workspaces.sh
+++ workspaces/refresh_other_workspaces.sh
@@ -3,13 +3,13 @@
 function main {
 	for dir in "./"*
 	do
-		if [ ${dir} = "./sample-workspace" ] ||
+		if [ ${dir} = "./liferay-sample-workspace" ] ||
 		   [ -f ${dir} ]
 		then
 			continue
 		fi
 
-		rsync -a --delete --exclude "client-extensions" --exclude "modules" --exclude "themes" sample-workspace/ ${dir}
+		rsync -a --delete --exclude "client-extensions" --exclude "modules" --exclude "themes" liferay-sample-workspace/ ${dir}
 	done
 }
 
