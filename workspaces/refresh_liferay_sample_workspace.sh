40c2d02f038710340803f4fa79dfd85444aea538
Brian Chan <brian.chan@liferay.com>
2023-03-16 15:00:55 -0300

    ISSITES-5302 LPS-172374 Upgrade

--- workspaces/refresh_liferay_sample_workspace.sh
+++ workspaces/refresh_liferay_sample_workspace.sh
@@ -62,7 +62,7 @@ function refresh_liferay_sample_workspace {
 
 	mv gradle.properties.tmp gradle.properties
 
-	sed -i 's/name: "com.liferay.gradle.plugins.workspace", version: ".*"/name: "com.liferay.gradle.plugins.workspace", version: "4.1.22"/' settings.gradle
+	sed -i 's/name: "com.liferay.gradle.plugins.workspace", version: ".*"/name: "com.liferay.gradle.plugins.workspace", version: "4.1.25"/' settings.gradle
 
 	popd
 
